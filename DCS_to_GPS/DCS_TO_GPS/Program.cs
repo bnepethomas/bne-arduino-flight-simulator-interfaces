f2092710140f21a2da57560c5bc4503319c723f8
bnepethomas <bnepethomas@gmail.com>
2017-05-11 18:02:24 +1000

    Early Days

---
+++ DCS_to_GPS/DCS_TO_GPS/Program.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using System.IO.Ports;
+
+namespace DCS_TO_GPS
+{
+    
+
+
+    class Program
+    {
+        
+        static public string GPSComPort;
+        static public ProgramMode OperationalMode;
+        
+        static public double gLat = 0;     // N/S to five decimal places
+        static public double gLong = 0;    // E/W to five decimal places
+
+        static public string xoutputstr;
+        static public string youtputstr;
+
+        static public String outNorS;
+        static public String outEorW;
+
+        static public string outSpeed = "100";
+        static public string outAltitude = "1000";
+        static public string outLatitude = "44.89";
+        static public string outLongitude = "35.39";
+        static public string outTrackMadeGood = "000";
+        static public string outMagVar = "00.0";
+
+        static public double Lomac_GPS_Altitude = 100;
+        static public double Lomac_GPS_Heading = 90;
+        static public double Lomac_GPS_Latitude = -27.3873;
+        static public double Lomac_GPS_Longitude = 153.1301;
+        static public double Lomac_GPS_Speed = 333;
+
+
+        public enum ProgramMode
+        {
+            DemoMode,
+            F4Mode,
+            LomacMode
+        };
+        
+        static void Main(string[] args)
+        {
+            DCS_TO_GPSOut_init();
+        }
+
+        static void DCS_TO_GPSOut_init()
+        {
+            foreach (string arg in Environment.GetCommandLineArgs())
+            {
+                if (arg.IndexOf("Falcon") != -1)
+                {
+                    OperationalMode = ProgramMode.F4Mode;
+                    break;
+                }
+                else if (arg.IndexOf("Lomac") != -1)
+                {
+                    OperationalMode = ProgramMode.LomacMode;
+                    break;
+                }
+                else if (arg.IndexOf("reset") != -1)
+                {
+                    Console.WriteLine("Reseting Configuration to default");
+                    Console.ReadLine();   
+                    Properties.Settings.Default.GPSComPort = "Com0";
+                    Properties.Settings.Default.Save();
+                }
+
+                
+            }
+
+
+
+            GPSComPort = Properties.Settings.Default.GPSComPort;
+
+
+
+            bool bFoundPort = false;
+            foreach (string s in SerialPort.GetPortNames())
+            {
+                if (0 == String.Compare(s, GPSComPort, true))
+                {
+                    bFoundPort = true;
+                    Console.WriteLine("Using ");
+                }
+            }
+
+
+            if (!bFoundPort)
+            {
+                
+                string strYorN;
+                Console.WriteLine("Unable to find configured Serial Port " + GPSComPort);
+                Console.WriteLine("Press Enter to Continue");
+                Console.ReadLine();
+                Console.WriteLine("Installed Serial Ports");
+                foreach (string s in SerialPort.GetPortNames())
+                {
+                    Console.Write(s);
+                    Console.Write(" Use this port (Y/N): ");
+                    strYorN = Console.ReadLine();
+                    if (strYorN.Contains("Y") || strYorN.Contains("y"))
+                    {
+                        Console.WriteLine("Saving GPS Serial Port " + s);
+                        Properties.Settings.Default.GPSComPort = s;
+                        Properties.Settings.Default.Save();
+                        Console.WriteLine("Press Enter to Continue");
+                        Console.ReadLine();            
+                        break;
+                    }
+ 
+                }
+                Console.WriteLine("Press Enter to Continue");
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ DCS_to_GPS/DCS_TO_GPS/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("DCS_TO_GPS")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DCS_TO_GPS")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("6eb0fd74-23e1-4414-af1a-87cd1ebaa865")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
