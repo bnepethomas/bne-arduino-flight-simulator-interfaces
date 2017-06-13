ed91075df37fa91a0cd304dc37f1b08b953d79d3
bnepethomas <bnepethomas@gmail.com>
2017-06-13 19:28:59 +1000

    adding udp test and receive framework

---
+++ Huey Caution Panel/receiver_001.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+
+import socket
+
+UDP_IP = "127.0.0.1"
+UDP_PORT = 5005
+
+sock = socket.socket(socket.AF_INET, # Internet
+                     socket.SOCK_DGRAM) # UDP
+sock.bind((UDP_IP, UDP_PORT))
+
+while True:
+    try:
+       while True:
+          sock.settimeout(2)
+          data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
+          print "received message:", data
+
+    except socket.timeout:
+	print("timeout")
+        continue
+
+
