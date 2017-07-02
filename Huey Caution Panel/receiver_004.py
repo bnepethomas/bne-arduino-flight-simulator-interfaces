385304af907a63d94a8158b4b8d02c41137f0b52
bnepethomas <bnepethomas@gmail.com>
2017-07-02 12:50:12 +1000

    g

--- Huey Caution Panel/receiver_004.py
+++ Huey Caution Panel/receiver_004.py
@@ -68,28 +68,27 @@
 #39 	GND 	GND 	
 #40 	GPIO21 	D18
 
-
 # Led Mappings (x,y)
-# 1,7   lamp_ENGINE_OIL_PRESS
-# 1,6   lamp_ENGINE_ICING
-# 1,5   lamp_ENGINE_ICE_DET
-# 1,4   lamp_ENGINE_CHIP_DET
-# 1,3   lamp_LEFT_FUEL_BOOST
-# 1,1   lamp_RIGHT_FUEL_BOOST
-# 1,2   lamp_ENG_FUEL_PUMP
-# 1,0   lamp_20_MINUTE
-# 3,1   lamp_FUEL_FILTER
-# 3,0   lamp_GOV_EMERG
-# 2,7   lamp_AUX_FUEL_LOW
-# 2,6   lamp_XMSN_OIL_PRESS
-# 2,5   lamp_XMSN_OIL_HOT
-# 2,4   lamp_HYD_PRESSURE
-# 2,3   lamp_ENGINE_INLET_AIR
-# 2,2   lamp_INST_INVERTER
-# 2,1   lamp_DC_GENERATOR
-# 2,0   lamp_EXTERNAL_POWER
-# 4,1   lamp_CHIP_DETECTOR
-# 4,0   lamp_IFF
+# 0,7   91  lamp_ENGINE_OIL_PRESS
+# 0,6   92  lamp_ENGINE_ICING
+# 0,5   93  lamp_ENGINE_ICE_DET
+# 0,4   94  lamp_ENGINE_CHIP_DET
+# 0,3   95  lamp_LEFT_FUEL_BOOST
+# 0,1   96  lamp_RIGHT_FUEL_BOOST
+# 0,2   97  lamp_ENG_FUEL_PUMP
+# 0,0   98  lamp_20_MINUTE
+# 2,1   99  lamp_FUEL_FILTER
+# 2,0   100 lamp_GOV_EMERG
+# 1,7   101 lamp_AUX_FUEL_LOW
+# 1,6   102 lamp_XMSN_OIL_PRESS
+# 1,5   103 lamp_XMSN_OIL_HOT
+# 1,4   104 lamp_HYD_PRESSURE
+# 1,3   105 lamp_ENGINE_INLET_AIR
+# 1,2   106 lamp_INST_INVERTER
+# 1,1   107 lamp_DC_GENERATOR
+# 1,0   108 lamp_EXTERNAL_POWER
+# 3,1   109 lamp_CHIP_DETECTOR
+# 3,0   110 lamp_IFF
 
 
 
@@ -116,7 +115,7 @@ UDP_PORT = 7784
 
 def checkandassign(attribname, attribvalue):
     #print "checking value"
-    if attribname == "300":
+    if attribname == "91":
         print "found it 300"
     elif attribname == "301":
         print "foundit 301"
@@ -187,7 +186,7 @@ def Ledalloff():
     print("Finished Leds all off")  
 
 def DensitySweep():
-    for _ in range(4):
+    for _ in range(3):
         for intensity in range(16):
             device.contrast(intensity * 16)
             time.sleep(0.1)
@@ -203,16 +202,17 @@ print "Created Max7219 Device"
 
 # Excerise Leds
 # Random led pattern
-LedRandom()
-time.sleep(1)
+
+#LedRandom()
+#time.sleep(1)
 # All on
-Ledallon()
-time.sleep(1)
+#Ledallon()
+#time.sleep(1)
 # Density sweep
-DensitySweep()
+#DensitySweep()
 # All off
 Ledalloff()
-time.sleep(1)
+#time.sleep(1)
 
 
 sock = socket.socket(socket.AF_INET, # Internet
@@ -233,21 +233,163 @@ while True:
           #print "received message:", data
           words = data.split(":")
           #print words
-          for current_word in words:
-              print(current_word)
-              #print(len(current_word))
+          with canvas(device) as draw:
+            for current_word in words:
+                  print(current_word)
+                  #print(len(current_word))
 
-              # Basic sanity check to catch values that are too short
-              if len(current_word) >= 3:
-                  values = current_word.split("=")
-                  print values
-                  #print values[0] + "-" + values[1]
-                  checkandassign( values[0], values[1])
-                  for current_value in values:
-                      
-                      if current_value == "300":
-                          print "Handling 300"
-                          demo(1, 0)
+                  # Basic sanity check to catch values that are too short
+                  if len(current_word) >= 3:
+                      values = current_word.split("=")
+                      print values
+                      print values[0] + "-" + values[1]
+                      #checkandassign( values[0], values[1])
+                      if values[0] == '91':
+                            print "Handling 91-lamp_ENGINE_OIL_PRESS"
+                            if values[1] == "1":
+                                draw.point((0, 7 ), 1)
+                            else:
+                                draw.point((0, 7 ), 0)
+                      if values[0] == '92':
+                            print "Handling 92-lamp_ENGINE_ICING"
+                            if values[1] == "1":
+                                draw.point((0, 6 ), 1)
+                            else:
+                                draw.point((0, 6 ), 0) 
+                      if values[0] == '93':
+                            print "Handling 93-lamp_ENGINE_ICE_DET"
+                            if values[1] == "1":
+                                draw.point((0, 5 ), 1)
+                            else:
+                                draw.point((0, 5 ), 0)                             
+                      if values[0] == '94':
+                            print "Handling 94-lamp_ENGINE_CHIP_DET"
+                            if values[1] == "1":
+                                draw.point((0, 4 ), 1)
+                            else:
+                                draw.point((0, 4 ), 0)
+                      if values[0] == '95':
+                            print "Handling 95-lamp_LEFT_FUEL_BOOST"
+                            if values[1] == "1":
+                                draw.point((0, 3 ), 1)
+                            else:
+                                draw.point((0, 3 ), 0)                                
+                      if values[0] == '96':
+                            print "Handling 96-lamp_RIGHT_FUEL_BOOST"
+                            if values[1] == "1":
+                                draw.point((0, 1 ), 1)
+                            else:
+                                draw.point((0, 1 ), 0)                                 
+
+                      if values[0] == '97':
+                            print "Handling 97-lamp_ENG_FUEL_PUMP"
+                            if values[1] == "1":
+                                draw.point((0, 2 ), 1)
+                            else:
+                                draw.point((0, 2 ), 0)
+                      if values[0] == '98':
+                            print "Handling 98-lamp_20_MINUTE"
+                            if values[1] == "1":
+                                draw.point((0, 0 ), 1)
+                            else:
+                                draw.point((0, 0 ), 0)
+                      if values[0] == '99':
+                            print "Handling 99-lamp_FUEL_FILTER"
+                            if values[1] == "1":
+                                draw.point((2, 1 ), 1)
+                            else:
+                                draw.point((2, 1 ), 0)                                
+                      if values[0] == '100':
+                            print "Handling 100-lamp_GOV_EMERG"
+                            if values[1] == "1":
+                                draw.point((2, 0 ), 1)
+                            else:
+                                draw.point((2, 0 ), 0)                                
+                      if values[0] == '101':
+                            print "Handling 101-lamp_AUX_FUEL_LOW"
+                            if values[1] == "1":
+                                draw.point((1, 7 ), 1)
+                            else:
+                                draw.point((1, 7 ), 0)  
+                      if values[0] == '102':
+                            print "Handling 102-lamp_XMSN_OIL_PRESS"
+                            if values[1] == "1":
+                                draw.point((1, 6 ), 1)
+                            else:
+                                draw.point((1, 6 ), 0)
+                      if values[0] == '103':
+                            print "Handling 103-lamp_XMSN_OIL_HOT"
+                            if values[1] == "1":
+                                draw.point((1, 5 ), 1)
+                            else:
+                                draw.point((1, 5 ), 0)
+                      if values[0] == '104':
+                            print "Handling 104-lamp_HYD_PRESSURE"
+                            if values[1] == "1":
+                                draw.point((1, 4 ), 1)
+                            else:
+                                draw.point((1, 4 ), 0)
+                      if values[0] == '105':
+                            print "Handling 105-lamp_ENGINE_INLET_AIR"
+                            if values[1] == "1":
+                                draw.point((1, 3 ), 1)
+                            else:
+                                draw.point((1, 3 ), 0)
+                      if values[0] == '106':
+                            print "Handling 106-lamp_INST_INVERTER"
+                            if values[1] == "1":
+                                draw.point((1, 2 ), 1)
+                            else:
+                                draw.point((1, 2 ), 0)
+                      if values[0] == '107':
+                            print "Handling 107-lamp_DC_GENERATOR"
+                            if values[1] == "1":
+                                draw.point((1, 1 ), 1)
+                            else:
+                                draw.point((1, 1 ), 0)
+                      if values[0] == '108':
+                            print "Handling 108-lamp_EXTERNAL_POWER"
+                            if values[1] == "1":
+                                draw.point((1, 0 ), 1)
+                            else:
+                                draw.point((1, 0 ), 0)
+                      if values[0] == '109':
+                            print "Handling 109-lamp_CHIP_DETECTOR"
+                            if values[1] == "1":
+                                draw.point((3, 1 ), 1)
+                            else:
+                                draw.point((3, 1 ), 0)
+                      if values[0] == '110':
+                            print "Handling 110-lamp_IFF"
+                            if values[1] == "1":
+                                draw.point((3, 0 ), 1)
+                            else:
+                                draw.point((3, 0 ), 0)
+# Led Mappings (x,y)
+# 0,7   91  lamp_ENGINE_OIL_PRESS
+# 0,6   92  lamp_ENGINE_ICING
+# 0,5   93  lamp_ENGINE_ICE_DET
+# 0,4   94  lamp_ENGINE_CHIP_DET
+# 0,3   95  lamp_LEFT_FUEL_BOOST
+# 0,1   96  lamp_RIGHT_FUEL_BOOST
+# 0,2   97  lamp_ENG_FUEL_PUMP
+# 0,0   98  lamp_20_MINUTE
+# 2,1   99  lamp_FUEL_FILTER
+# 2,0   100 lamp_GOV_EMERG
+# 1,7   101 lamp_AUX_FUEL_LOW
+# 1,6   102 lamp_XMSN_OIL_PRESS
+# 1,5   103 lamp_XMSN_OIL_HOT
+# 1,4   104 lamp_HYD_PRESSURE
+# 1,3   105 lamp_ENGINE_INLET_AIR
+# 1,2   106 lamp_INST_INVERTER
+# 1,1   107 lamp_DC_GENERATOR
+# 2,0   108 lamp_EXTERNAL_POWER
+# 4,1   109 lamp_CHIP_DETECTOR
+# 4,0   110 lamp_IFF
+
+                          
+                            
+
 
     except socket.timeout:
 	print("timeout")
