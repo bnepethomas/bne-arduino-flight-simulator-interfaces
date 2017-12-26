8a8ceba75c12f7434c8130483ffdecc0509c4320
bnepethomas <bnepethomas@gmail.com>
2017-12-26 14:48:59 +1000

    hundreds rolling

--- DevPlayArea/ssd1306oled.py
+++ DevPlayArea/ssd1306oled.py
@@ -28,6 +28,16 @@ hatch_height = 20
 
 character_width = 12
 
+cursor_Multiplier = 2.5
+
+top = 0
+x = 23
+top_hidden_row = -27
+top_row = -6
+middle_row = 15
+bottom_row = 36
+bottom_hidden_row = 57
+
 def DrawHatch():
    
     # Darw White box
@@ -57,21 +67,62 @@ def DrawThousands(alt_ThousandsValue):
 
 
 
+
+
+
 def DrawHundreds(alt_HundredsValue, alt_TensValue):
-    # Large Rectangle to cover all three rows
-    draw.rectangle((hundred_Column_Pos, 0, hundred_Column_Pos + column_Spacing, 60), outline=colour_black, fill=colour_black)
 
-    if (alt_HundredsValue != 0):
-        draw.text((hundred_Column_Pos, top_row ),str(alt_HundredsValue-1),  font=font, fill=colour_white)
-    else:
-        draw.text((hundred_Column_Pos, top_row ),'9',  font=font, fill=colour_white)
+    ###
+##    display.setCursor(ten_Column_Pos,16 +  ones_converted * cursor_multiplier);
+##    display.println((tens + 9) % 10);
+##  
+##  
+##
+##    display.setCursor(ten_Column_Pos,42 +  ones_converted * cursor_multiplier);
+##    display.println(tens);
+##  
+##  
+##    // At the bottom of the screen
+##    // Orginal Font
+##    //display.setCursor(35,45 +  ones_converted * cursor_multiplier);
+##    // New Font
+##    display.setCursor(ten_Column_Pos,66 +  ones_converted * cursor_multiplier);
+##    display.println((tens + 1) % 10);
+##  
+##  
+##    // At the bottom below the screen
+##    // Orginal Font
+##    //display.setCursor(35,69 +  ones_converted * cursor_multiplier);
+##    // New Font
+##    display.setCursor(ten_Column_Pos,90 +  ones_converted * cursor_multiplier);
+##    display.println((tens + 2) % 10);
+##  
+##    // Remove bottom of last character to remove distraction of digit appearing and disappearing
+##    // Orginal Font
+##    //display.fillRect(35, 65, 15, 25, BLACK);
+##    // New Font
+##    display.fillRect(ten_Column_Pos, 65, 20, 27, BLACK);
+
+    ####################################
+
+    vertical_offset = 0
+    tens_Corrected = (alt_TensValue * cursor_Multiplier * -1) + 23
     
-    draw.text((hundred_Column_Pos, middle_row), str(alt_HundredsValue), font=font, fill=colour_white)
+    # Large Rectangle to cover all three rows
+    draw.rectangle((hundred_Column_Pos, 0, hundred_Column_Pos + column_Spacing, 64), outline=colour_black, fill=colour_black)
 
-    if (alt_HundredsValue != 9):
-        draw.text((hundred_Column_Pos, bottom_row ), str(alt_HundredsValue+1), font=font, fill=colour_white)
-    else:
-        draw.text((hundred_Column_Pos, bottom_row ), '0', font=font, fill=colour_white)
+    draw.text((hundred_Column_Pos, top_hidden_row + tens_Corrected ), str((alt_HundredsValue+8) % 10), font=font, fill=colour_white)
+
+    draw.text((hundred_Column_Pos, top_row + tens_Corrected),str((alt_HundredsValue+9) % 10),  font=font, fill=colour_white)
+
+    
+    draw.text((hundred_Column_Pos, middle_row + tens_Corrected), str(alt_HundredsValue), font=font, fill=colour_white)
+
+
+    draw.text((hundred_Column_Pos, bottom_row + tens_Corrected ), str((alt_HundredsValue+1) % 10), font=font, fill=colour_white)
+
+    draw.text((hundred_Column_Pos, bottom_hidden_row + tens_Corrected ), str((alt_HundredsValue+2) % 10), font=font, fill=colour_white)
+
     
 
 # Raspberry Pi pin configuration:
@@ -118,11 +169,8 @@ font = ImageFont.load_default()
 font = ImageFont.truetype('monofonto.ttf', 26)
 
 # Write two lines of text.
-top = 0
-x = 23
-top_row = -6
-middle_row = 15
-bottom_row = 36
+
+
 draw.text((thousand_Column_Pos, middle_row), '3', font=font, fill=colour_white)
 draw.text((hundred_Column_Pos, top_row ),'6',  font=font, fill=colour_white)
 draw.text((hundred_Column_Pos, middle_row),'7', font=font, fill=colour_white)
@@ -169,7 +217,17 @@ for i in range(0,10):
 
 for k in range(1,3):    
     for i in range(0,10):
-        time.sleep(0.1)
-        DrawHundreds(i,0)
-        disp.image(image)
-        disp.display()
+        for j in range(0,10):
+            #ime.sleep(0.1)
+            DrawHundreds(i,j)
+            disp.image(image)
+            disp.display()
+            #print(i,j)
+
+for k in range(1,3):    
+    for i in range(9,0, -1):
+        for j in range(9,0,-1):
+            #time.sleep(0.3)
+            DrawHundreds(i,j)
+            disp.image(image)
+            disp.display()
