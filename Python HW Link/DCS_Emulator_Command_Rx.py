4648aa45fc54bd7240d8ba84609c87e06e0b518d
pethomas <pethomas@cisco.com>
2018-05-11 16:03:50 +0200

    work

---
+++ Python HW Link/DCS_Emulator_Command_Rx.py
@@ -0,0 +1,21 @@
+import socket
+
+UDP_IP_ADDRESS = "127.0.0.1"
+UDP_PORT_NO = 26027
+serverSock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+serverSock.settimeout(0.0001)
+serverSock.bind((UDP_IP_ADDRESS, UDP_PORT_NO))
+
+a=0
+while True:
+    
+    try:
+        data, addr = serverSock.recvfrom(1024)
+        print ("Message: ", data)
+	print(a)
+	a=0
+
+                                          
+    except socket.timeout:
+        a=a+1
+        continue
--- Python HW Link/PyHWLink_Pri_Node_Output.py
+++ Python HW Link/PyHWLink_Pri_Node_Output.py
@@ -59,17 +59,18 @@ def Send_UDP_Command(command_to_send):
     # Send Command to IKARUS in DCS
     # The target port is found in config.lua - ExportScript.Config.ListenerPort
     # We determine where to send the update based on the last data packet we received
-    UDP_IP = Last_Source_IP
-    UDP_PORT = 26027   
+    UDP_IP = "127.0.0.1"
+    TX_UDP_PORT = 26027   
 
 
-    print ("UDP target IP:"), UDP_IP
-    print ("UDP target port:"), UDP_PORT
+    print ("UDP target IP:", UDP_IP)
+    print ("UDP target port:", TX_UDP_PORT)
 
-    sock = socket.socket(socket.AF_INET, # Internet
+    txsock = socket.socket(socket.AF_INET, # Internet
                      socket.SOCK_DGRAM) # UDP
-    sock.sendto(command_to_send, (UDP_IP, UDP_PORT))
-
+    print ("step")                 
+    #sock.sendto(command_to_send, (UDP_IP, TX_UDP_PORT))
+    txsock.sendto("HI", (UDP_IP, TX_UDP_PORT))
 
 
 
@@ -82,9 +83,6 @@ def SendAllSwitchStates():
 
 def Reboot():
     print ("Received a Reboot - rebooting")
-
-    time.sleep(1)
-    GPIO.cleanup()       # clean up GPIO on CTRL+C exit
     print ("Here's what would have happened - os.system('shutdown now -r')")
     #os.system("shutdown now -r")
     sys.exit(0)
@@ -110,6 +108,8 @@ sock.bind((UDP_IP, UDP_PORT))
 while True:
     try:
        while True:
+          Send_UDP_Command("C15,3004,-1")
+          
           sock.settimeout(2)
           data, (Source_IP, Source_Port) = sock.recvfrom(1024) # buffer size is 1024 bytes
           if (Source_IP != Last_Source_IP):
@@ -117,167 +117,50 @@ while True:
             print ("New Source Address Found")
             SendAllSwitchStates()
           
+          
+          
           #print "received message:", data
           words = data.split(":")
           #print words
 
 
-          with canvas(device) as draw:
-            for current_word in words:
-                  #print(current_word)
-                  #print(len(current_word))
 
-                  # Basic sanity check to catch values that are too short
-                  if len(current_word) >= 3:
-                      values = current_word.split("=")
-                      #print values
-                      #print values[0] + "+" + values[1]
+          for current_word in words:
+                #print(current_word)
+                #print(len(current_word))
 
-                      if values[0] == '91':
-                            #print "Handling 91-lamp_ENGINE_OIL_PRESS"
-                            if values[1] == "1":
-                                draw.point((0, 7 ), 1)
-                            else:
-                                draw.point((0, 7 ), 0)
+                # Basic sanity check to catch values that are too short
+                if len(current_word) >= 3:
+                    values = current_word.split("=")
+                    #print values
+                    #print values[0] + "+" + values[1]
+
+                    if values[0] == '91':
+                        print ("Handling 91-lamp_ENGINE_OIL_PRESS")
 
 
-                      if values[0] == '92':
-                            #print "Handling 92-lamp_ENGINE_ICING"
-                            if values[1] == "1":
-                                draw.point((0, 6), 1)
-                            else:
-                                draw.point((0, 6), 0)
+                    if values[0] == '999':
+                        print ("Handling SHUTDOWN")
+                        if values[1] == "ShutdownAndHalt":
+                            # Stop listening on original port
+                            sock.close()
+                            ShutdownAndHalt()
+                        elif values[1] == "Reboot":
+                            # Stop listening on original port
+                            sock.close()
+                            Reboot()                                      
+                        else:
+                            print ("Received a Invalid Shutdown Request")
 
 
-                      if values[0] == '93':
-                            #print "Handling 93-lamp_ENGINE_ICE_DET"
-                            if values[1] == "1":
-                                draw.point((0, 5 ), 1)
-                            else:
-                                draw.point((0, 5 ), 0)                             
-                      if values[0] == '94':
-                            #print "Handling 94-lamp_ENGINE_CHIP_DET"
-                            if values[1] == "1":
-                                draw.point((0, 4 ), 1)
-                            else:
-                                draw.point((0, 4 ), 0)
-                      if values[0] == '95':
-                            #print "Handling 95-lamp_LEFT_FUEL_BOOST"
-                            if values[1] == "1":
-                                draw.point((0, 3 ), 1)
-                            else:
-                                draw.point((0, 3 ), 0)                                
-                      if values[0] == '96':
-                            #print "Handling 96-lamp_RIGHT_FUEL_BOOST"
-                            if values[1] == "1":
-                                draw.point((0, 1 ), 1)
-                            else:
-                                draw.point((0, 1 ), 0)                                 
-
-                      if values[0] == '97':
-                            #print "Handling 97-lamp_ENG_FUEL_PUMP"
-                            if values[1] == "1":
-                                draw.point((0, 2 ), 1)
-                            else:
-                                draw.point((0, 2 ), 0)
-                      if values[0] == '98':
-                            #print "Handling 98-lamp_20_MINUTE"
-                            if values[1] == "1":
-                                draw.point((0, 0 ), 1)
-                            else:
-                                draw.point((0, 0 ), 0)
-                      if values[0] == '99':
-                            #print "Handling 99-lamp_FUEL_FILTER"
-                            if values[1] == "1":
-                                draw.point((2, 1 ), 1)
-                            else:
-                                draw.point((2, 1 ), 0)                                
-                      if values[0] == '100':
-                            #print "Handling 100-lamp_GOV_EMERG"
-                            if values[1] == "1":
-                                draw.point((2, 0 ), 1)
-                            else:
-                                draw.point((2, 0 ), 0)                                
-                      if values[0] == '101':
-                            #print "Handling 101-lamp_AUX_FUEL_LOW"
-                            if values[1] == "1":
-                                draw.point((1, 7 ), 1)
-                            else:
-                                draw.point((1, 7 ), 0)  
-                      if values[0] == '102':
-                            #print "Handling 102-lamp_XMSN_OIL_PRESS"
-                            if values[1] == "1":
-                                draw.point((1, 6 ), 1)
-                            else:
-                                draw.point((1, 6 ), 0)
-                      if values[0] == '103':
-                            #print "Handling 103-lamp_XMSN_OIL_HOT"
-                            if values[1] == "1":
-                                draw.point((1, 5 ), 1)
-                            else:
-                                draw.point((1, 5 ), 0)
-                      if values[0] == '104':
-                            #print "Handling 104-lamp_HYD_PRESSURE"
-                            if values[1] == "1":
-                                draw.point((1, 4 ), 1)
-                            else:
-                                draw.point((1, 4 ), 0)
-                      if values[0] == '105':
-                            #print "Handling 105-lamp_ENGINE_INLET_AIR"
-                            if values[1] == "1":
-                                draw.point((1, 3 ), 1)
-                            else:
-                                draw.point((1, 3 ), 0)
-                      if values[0] == '106':
-                            #print "Handling 106-lamp_INST_INVERTER"
-                            if values[1] == "1":
-                                draw.point((1, 2 ), 1)
-                            else:
-                                draw.point((1, 2 ), 0)
-                      if values[0] == '107':
-                            #print "Handling 107-lamp_DC_GENERATOR"
-                            if values[1] == "1":
-                                draw.point((1, 1 ), 1)
-                            else:
-                                draw.point((1, 1 ), 0)
-                      if values[0] == '108':
-                            #print "Handling 108-lamp_EXTERNAL_POWER"
-                            if values[1] == "1":
-                                draw.point((1, 0 ), 1)
-                            else:
-                                draw.point((1, 0 ), 0)
-                      if values[0] == '109':
-                            #print "Handling 109-lamp_CHIP_DETECTOR"
-                            if values[1] == "1":
-                                draw.point((3, 1 ), 1)
-                            else:
-                                draw.point((3, 1 ), 0)
-                      if values[0] == '110':
-                            #print "Handling 110-lamp_IFF"
-                            if values[1] == "1":
-                                draw.point((3, 0 ), 1)
-                            else:
-                                draw.point((3, 0 ), 0)
-                      if values[0] == '999':
-                            print ("Handling SHUTDOWN")
-                            if values[1] == "ShutdownAndHalt":
-                                # Stop listening on original port
-                                sock.close()
-                                ShutdownAndHalt()
-                            elif values[1] == "Reboot":
-                                # Stop listening on original port
-                                sock.close()
-                                Reboot()                                      
-                            else:
-                                print ("Received a Invalid Shutdown Request")
-
-
-        
+    
 
 
     except socket.timeout:
         print("timeout")
+        
         continue
+        
 
     except KeyboardInterrupt:
         # Catch Ctl-C and quit
