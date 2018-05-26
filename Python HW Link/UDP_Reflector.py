d2338cab22dd5ea5879167080d940ab50c324b5b
pethomas <pethomas@cisco.com>
2018-05-26 20:04:43 +0200

    adding cmd line options

--- Python HW Link/UDP_Reflector.py
+++ Python HW Link/UDP_Reflector.py
@@ -7,17 +7,9 @@
 # By default displays all packets but will filter using wireshark filter (ie ip.addr==X.X.X.X)
 #
 #
-# Interactive options
+# Command line options
 #
-# ip.addr==X.X.X.X (no spaces)
-#   only display packets from given address (X.X.X.X)
-#   display packets from all sources (*)
-# f==X.X.X.X
-#   forward UDP packets to fixed port
-
-
-#a = raw_input("need to insert leading zeros so sort is correct enter value and press enter to continue: ")
-
+# filterstring - display packets containing this string
 
 
 
@@ -25,15 +17,37 @@ import os
 import socket
 import sys
 import time
+import threading
+
 
 # Used for command line parsing
 from optparse import OptionParser
+
+
+def CleanUpAndExit():
+    try:
+        # Catch Ctl-C and quit
+        print('')
+        print('')
+        print('Exiting')
+        try:
+            serverSock.close()
+        except:
+            print('[i] Unable to close server socket')
+        sys.exit(0)
+
+    except Exception as other:
+        print(time.asctime() + "[e] Error in CleanUpAndExit: " + str(other))
+        sys.exit(0)
 
 
 
 # Global Variables
 debugging = False
 config_file = 'UDP_Reflector_config.py'
+filterstring = ''
+
+# Initialise keepalive indicator
 last_time_display = time.time()
 
 # Address and Port to listen on
@@ -44,75 +58,85 @@ UDP_Port = 27000
 wireshark_IP_Address = "127.0.0.1"
 wireshark_Port = 27001
 
-# See if input configuration file exists
-# Parameters are either specified in this file or passed via command line
-# Command line parameters override any settings in the config file
-#
+try:
 
-if not (os.path.isfile(config_file)):
-    
-    print('Unable to find ' + config_file)
-    
-else:
-    try:
-        from config_file import *
+    if not (os.path.isfile(config_file)):
         
+        print('[i] Unable to find ' + config_file)
+
+        
+    else:
+        try:
+            from config_file import *
             
-    except:
-        print('Unable to open "' + config_file + '"' )
-        print('Or variable assignment incorrect - forgot quotes for string?')
-        print('Defaults used')
+                
+        except Exception as other:
+            print(time.asctime() + "[e] Error in Initialisation: " + str(other))
+
+            print('Unable to open "' + config_file + '"' )
+            print('Or variable assignment incorrect - forgot quotes for string?')
+            print('Defaults used')
+
+    # The timeout is aggressive
+    serverSock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    serverSock.settimeout(0.0001)
+    serverSock.bind((UDP_IP_Address, UDP_Port))
+
+    wireshark_Sock = socket.socket(socket.AF_INET, # Internet
+        socket.SOCK_DGRAM) # UDP
+
+
+
+    # See if input configuration file exists
+    # Parameters are either specified in this file or passed via command line
+    # Command line parameters override any settings in the config file
+    #
+
+
+
+
+    # See if value is assigned.  First we checked config file and then
+    #   command line arguments
+
+    if debugging: print("Checking Command Line parameters")
+
+
+    parser = OptionParser()
+    parser.add_option("-d","--debug", dest="optiondebug",action="store_true",
+                      default=False,
+                      help="enable debug",metavar="DEBUGLEVEL")
     
 
+    (options, args) = parser.parse_args()
+
+    if debugging: print("options:" + str(options))
+    if debugging: print("arguments:" +  str(args))
+
+    if len(args) != 0:
+        filterString = args[0]
+        print("[i] Display Filter is :" + str(args[0]))            
 
 
 
-# See if value is assigned.  First we checked config file and then
-#   command line arguments
+    # Override Debug mode if passed via command line    
+    if options.optiondebug == True:
+        debugging = True
+        print('Debugging Mode enabled through command line')    
 
-if debugging: print("Checking Command Line parameters")
+except KeyboardInterrupt:
+    CleanUpAndExit()
 
-
-parser = OptionParser()
-parser.add_option("-d","--debug", dest="optiondebug",action="store_true",
-                  default=False,
-                  help="enable debug",metavar="DEBUGLEVEL")
-parser.add_option("-l","--learning", dest="optionLearning",action="store_true",
-                  default=False,
-                  help="learning mode, assign actions to switch movements")
-(options, args) = parser.parse_args()
-
-if debugging: print("options:", str(options))
-if debugging: print("arguments:", args)
-
-# Override Learning mode if passed via command line
-if options.optionLearning == True:
-    learning = True
-    print('Learning Mode enabled through command line')
-# Override Debug mode if passed via command line    
-if options.optiondebug == True:
-    debugging = True
-    print('Debugging Mode enabled through command line')    
-
-
-
-# The timeout is aggressive
-serverSock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-serverSock.settimeout(0.0001)
-serverSock.bind((UDP_IP_Address, UDP_Port))
-
-wireshark_Sock = socket.socket(socket.AF_INET, # Internet
-     socket.SOCK_DGRAM) # UDP
+except Exception as other:
+    print(time.asctime() + "[e] Error in Setup: " + str(other))
 
 
 
 
 def ReceivePacket():
 
-
+    
     global last_time_display
-    # 'a' keeps track of how long it has been since packet was received
-    a=0
+    iterations_Since_Last_Packet = 0
 
 
     while True:
@@ -120,8 +144,6 @@ def ReceivePacket():
         try:
 
             data, (Source_IP, Source_Port)  = serverSock.recvfrom(1500)
-
-
 
             ReceivedPacket = data
 
@@ -133,27 +155,28 @@ def ReceivePacket():
             if debugging: print ("From: " + Source_IP + " " + Source_Port)
             if debugging: print ("Message: " + ReceivedPacket)
 
-            ProcessReceivedString( str(ReceivedPacket), Source_IP , str(Source_Port))
+            ProcessReceivedString( str(ReceivedPacket), Source_IP , str(Source_Port) )
             
-            if debugging: print(a)
-            a=0
+            if debugging: print("Iterations since last packet " + str(iterations_Since_Last_Packet))
+            
+            iterations_Since_Last_Packet=0
 
                                               
         except socket.timeout:
-            a=a+1
-            if (a > 10000):
-                print("Mid Receive Timeout - ", time.asctime())
-                a=0
+            iterations_Since_Last_Packet = iterations_Since_Last_Packet +  1
+            if (iterations_Since_Last_Packet > 10000):
+                print("[i] Mid Receive Timeout - ", time.asctime())
+                iterations_Since_Last_Packet=0
             if time.time() - last_time_display > 5:
                 print('Keepalive check ' + time.asctime())
                 last_time_display = time.time()
             continue
 
-        except:
-            print('Error in ReceivePacket. Error is: ' + sys.exc_info() [0])
+        except Exception as other:
+            print(time.asctime() + "[e] Error in ReceivePacket: " + str(other))
 
         if time.time() - last_time_display > 5:
-            print('Keepalive check ' + time.asctime())
+            print('Keepalive ' + time.asctime())
             last_time_display = time.time()
 
 
@@ -170,47 +193,35 @@ def ProcessReceivedString(ReceivedUDPString, Source_IP, Source_Port):
             
             ReceivedUDPString = str(ReceivedUDPString)
             if debugging: print ("From: " + Source_IP + " " + Source_Port)
-            if debugging: print('Stage 1 Processing: ' + ReceivedUDPString)
-            Send_string = Source_IP + ':' + Source_Port + '---' + ReceivedUDPString
+            if debugging: print('Payload: ' + ReceivedUDPString)
+            Send_String = Source_IP + ':' + Source_Port + '---' + ReceivedUDPString
             
-            wireshark_Sock.sendto(Send_string, (wireshark_IP_Address, wireshark_Port))
+            wireshark_Sock.sendto(Send_String, (wireshark_IP_Address, wireshark_Port))
+            if filterString !="":
+                if filterString in Send_String:
+                    print(Send_String)
 
-          
-            
-    except:
-        print('Error in ProcessReceivedString. Error is: ' + sys.exc_info() [0])
-
-
-def RemoveUnwantedCharacters(stringToBeCleaned):
-    stringToBeCleaned= stringToBeCleaned.replace('"', '')
-    stringToBeCleaned = stringToBeCleaned.strip(' ')
-    return(stringToBeCleaned)
-
+    except Exception as other:
+        print(time.asctime() + "Error in ProcessReceivedString. Error is: " + str(other))          
+             
 
 
 
 def Main():
 
+
+
     print('Listening on port ' + str(UDP_Port))
     try:
-        print('Waiting for packet')
+
         ReceivePacket()
 
     except KeyboardInterrupt:
         # Catch Ctl-C and quit
-        print('Exiting')
+        CleanUpAndExit()
         
-        serverSock.close()
-
-        
-        sys.exit(0)
-
-    except:
-        print(time.asctime(), "Unexpected error:", sys.exc_info() [0])
-
-
-
-
+    except Exception as other:
+        print(time.asctime() + "[e] Error in Main: " + str(other))
 
 
 
