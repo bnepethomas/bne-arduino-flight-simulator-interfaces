00f3ff1e5197570a178525adb1752f6c7ca055be
bnepethomas <bnepethomas@gmail.com>
2018-12-02 16:52:40 +1000

    serial

--- Python HW Link/pyHWLink_Serial.py
+++ Python HW Link/pyHWLink_Serial.py
@@ -18,7 +18,8 @@ counter=0
 
 
 while 1:
-   ser.write(str.encode("the quick brown fox"))
+   ser.write(str.encode('$GPRMC,160533.00,A,1002.3552,N,01045.51552,E,400,0,010413,0,E*73\r\n'))
+
    time.sleep(1)
    print("hi")
    counter += 1
