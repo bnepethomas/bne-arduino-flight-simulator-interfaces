2752915ec845b8244beffae3b434780360737c5d
pethomas <pethomas@cisco.com>
2018-12-08 10:19:53 +1000

    hash

--- Python HW Link/pyHWLink_Serial.py
+++ Python HW Link/pyHWLink_Serial.py
@@ -1,25 +1,37 @@
 #!/usr/bin/env python
       
-
+import binascii
 import time
-import serial
-
-
-ser = serial.Serial(
-  
-   port='/dev/ttyS0',
-   baudrate = 9600,
-   parity=serial.PARITY_NONE,
-   stopbits=serial.STOPBITS_ONE,
-   bytesize=serial.EIGHTBITS,
-   timeout=1
-)
+# import serial
+# 
+# 
+# ser = serial.Serial(
+#   
+#    port='/dev/ttyS0',
+#    baudrate = 9600,
+#    parity=serial.PARITY_NONE,
+#    stopbits=serial.STOPBITS_ONE,
+#    bytesize=serial.EIGHTBITS,
+#    timeout=1
+# )
 counter=0
 
 
-while 1:
+while 0:
    ser.write(str.encode('$GPRMC,160533.00,A,1002.3552,N,01045.51552,E,400,0,010413,0,E*73\r\n'))
 
    time.sleep(1)
    print("hi")
    counter += 1
+   
+print("hello world") 
+workstring = "$GPRMC,160533.00,A,1002.3552,N,01045.51552,E,400,0,010413,0,E*"
+print(workstring)
+packet = workstring 
+checksum = 0
+for el in packet:
+    #print (hex((ord(el))))
+    print( binascii.hexlify(el))
+    checksum ^= hex(ord(el))
+
+print (checksum, hex(checksum), chr(checksum))
