ed1bc68d1c9ba1c5b6f4235998647f89bc77e854
bnepethomas <bnepethomas@gmail.com>
2018-12-08 11:40:47 +1000

    serial

--- Python HW Link/pyHWLink_Serial.py
+++ Python HW Link/pyHWLink_Serial.py
@@ -3,47 +3,47 @@
 import binascii
 import time
 import codecs
-# import serial
-# 
-# 
-# ser = serial.Serial(
-#   
-#    port='/dev/ttyS0',
-#    baudrate = 9600,
-#    parity=serial.PARITY_NONE,
-#    stopbits=serial.STOPBITS_ONE,
-#    bytesize=serial.EIGHTBITS,
-#    timeout=1
-# )
+import serial
+
+
+ser = serial.Serial(
+
+    port='/dev/ttyS0',
+    baudrate = 9600,
+    parity=serial.PARITY_NONE,
+    stopbits=serial.STOPBITS_ONE,
+    bytesize=serial.EIGHTBITS,
+    timeout=1
+)
 counter=0
 
+def CalcChecksum(strToCalc):
+    print(strToCalc)
+    packet = strToCalc.encode() 
+    checksum = 0
+    for el in packet:
+        checksum ^= el
 
-while 0:
-   ser.write(str.encode('$GPRMC,160533.00,A,1002.3552,N,01045.51552,E,400,0,010413,0,E*73\r\n'))
+    print (checksum, hex(checksum), chr(checksum))
+    #Need to convert integer/byte into 2 characters
+    mychecksum = hex(checksum)
+    print (mychecksum)
+    mychecksum = mychecksum[2:4]
+    print ('Checksum is ' +  mychecksum)
+    
+    return(mychecksum)
+    #ser.write(str.encode('$GPRMC,160533.00,A,1002.3552,N,01045.51552,E,400,0,010413,0,E*73\r\n'))
 
-   time.sleep(1)
-   print("hi")
-   counter += 1
-   
-print("hello world") 
+
 workstring = "GPRMC,160533.00,A,1002.3552,N,01045.51552,E,400,0,010413,0,E"
-print(workstring)
-packet = workstring.encode() 
-checksum = 0
-for el in packet:
-    print(el)
-    checksum ^= el
+workstring = "GPRMC,160533.00,A,2750.4500,S,15312.19000,E,400,0,010413,0,E"
 
-print (checksum, hex(checksum), chr(checksum))
-#Need to convert integer/byte into 2 characters
-mychecksum = hex(checksum)
-print (mychecksum)
-mychecksum = mychecksum + 'hi'
-mychecksum = mychecksum[2:4]
-print (mychecksum)
 
 ## Sucessfully calculated check sum by stripping leading $ and all characters post *
 # To output will need to prepend $, append * - the ASCII chars of the hex checksum and then CRLF
-outputstring = '$' + workstring + '*' + mychecksum
+outputstring = '$' + workstring + '*' + CalcChecksum(workstring) + '\r\n'
 print(outputstring)
+ser.write(str.encode(outputstring))
+
+
 
