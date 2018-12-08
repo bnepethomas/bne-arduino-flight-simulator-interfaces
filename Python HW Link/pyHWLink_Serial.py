ce5798abb27bd0d87c2053fc9af791a7ee5dab8a
bnepethomas <bnepethomas@gmail.com>
2018-12-09 08:22:44 +1000

    Progress

--- Python HW Link/pyHWLink_Serial.py
+++ Python HW Link/pyHWLink_Serial.py
@@ -30,6 +30,7 @@ def CalcChecksum(strToCalc):
     mychecksum = hex(checksum)
     # Remove leading '0x'
     mychecksum = mychecksum[2:4]
+    # NEMA Senstences like Caps - so ensure it is capitalised
     mychecksum = mychecksum.upper()
     print ('Checksum is ' +  mychecksum)
     
@@ -37,51 +38,78 @@ def CalcChecksum(strToCalc):
     #ser.write(str.encode('$GPRMC,160533.00,A,1002.3552,N,01045.51552,E,400,0,010413,0,E*73\r\n'))
 
 
-workstring = "GPRMC,160533.00,A,1002.3552,N,01045.51552,E,400,0,010413,0,E"
-workstring = "GPRMC,160533.00,A,2732.2500,S,15313.31000,E,299,0,010413,0,E"
-#workstring = "GPGGA,053302.00,2732.2500,S,15313.31000,E,1,05,0.0,131.9,M,,,,"
 
-outputstring = '$' + workstring + '*' + CalcChecksum(workstring) + '\r\n'
-print(outputstring)
-ser.write(str.encode(outputstring))
+
+# Reference - teststring
+if False:
+    workstring = "GPRMC,160533.00,A,2723.4120,S,15307.72900,E,299,0,010413,0,E"
+    # Checksum should be 63
+    outputstring = '$' + workstring + '*' + CalcChecksum(workstring) + '\r\n'
+    print(outputstring)
+    ser.write(str.encode(outputstring))
+
+outUTC = '160533.00'
+outDate = "010413"
+xoutputstr = '2723.4120'
+outNorS = 'S'
+youtputstr = '15307.72900'
+outEorW = 'E'
+
 
 ## Sucessfully calculated check sum by stripping leading $ and all characters post *
 # To output will need to prepend $, append * - the ASCII chars of the hex checksum and then CRLF
 
 #### From c#
+outStartOfString = 'GPRMC'
+outStatus = 'A'
+
+outSpeed = '299'
+outTrackMadeGood = '30'
+outMagVar = '0'
+outMagEorW = 'E'
+outEndOfString = '*'
+
+# Assemble string
+outcompletestring = outStartOfString + "," + outUTC + ","
+outcompletestring = outcompletestring + outStatus + ","
+outcompletestring = outcompletestring + xoutputstr + "," + outNorS + ","
+outcompletestring = outcompletestring + youtputstr + "," + outEorW + ","
+outcompletestring = outcompletestring + outSpeed + ","
+outcompletestring = outcompletestring + outTrackMadeGood + ","
+outcompletestring = outcompletestring + outDate + ","
+outcompletestring = outcompletestring + outMagVar + "," + outMagEorW 
+
+outputstring = '$' + outcompletestring + '*' + CalcChecksum(outcompletestring) + '\r\n'
+print(outputstring)
+ser.write(str.encode(outputstring))
+
+
+
+outStartOfString = "GPGGA"
+outGPSFix = "2";
+outNoofSatellites = "05"
+outPrecision = "0.0"
+outEndOfString = "*"
+
+outcompletestring = outStartOfString + "," + outUTC + ","
+outcompletestring = outcompletestring + xoutputstr + "," + outNorS + ","
+outcompletestring = outcompletestring + youtputstr + "," + outEorW + ","
+outcompletestring = outcompletestring + outGPSFix + ","
+outcompletestring = outcompletestring + outNoofSatellites + ","
+outcompletestring = outcompletestring + outPrecision + ","
+outcompletestring = outcompletestring + '6999' + ".0,M,,,,"
+outcompletestring = outcompletestring 
+
+
+outputstring = '$' + outcompletestring + '*' + CalcChecksum(outcompletestring) + '\r\n'
+print(outputstring)
+
+
 
 #  Out Satellite status
 outputstring = "GPGSA,A,3,01,02,03,,,,,,,,,,3.0,3.0,3.0,*"
 outputstring = '$' + outputstring + '*' +  CalcChecksum(outputstring) + '\r\n'
 ser.write(str.encode(outputstring))
-
-outStartOfString = "GPGGA";
-outUTC = "085823.00";
-outGPSFix = "2";
-outNoofSatellites = "05";
-outPrecision = "0.0";
-outEndOfString = "*";
-
-outcompletestring = outStartOfString + "," + outUTC + ",";
-outcompletestring = outcompletestring + '2732.2500' + "," + 'S' + ",";
-outcompletestring = outcompletestring + '15313.31000' + "," + 'E' + ",";
-outcompletestring = outcompletestring + outGPSFix + ",";
-outcompletestring = outcompletestring + outNoofSatellites + ",";
-outcompletestring = outcompletestring + outPrecision + ",";
-outcompletestring = outcompletestring + '6999' + ".0,M,,,,";
-outcompletestring = outcompletestring + outEndOfString;
-
-
-outputstring = '$' + outcompletestring + '*' + CalcChecksum(outcompletestring) + '\r\n'
-print(outputstring)
-#outputstring = '$GPGGA,092750.000,5321.6802,N,00630.3372,W,1,8,1.03,61.7,M,55.2,M,,*76' + '\r\n'
-#ser.write(str.encode(outputstring))
-
-
-#  Out Satellite status
-#outputstring = "GPGSA,A,3,01,02,03,,,,,,,,,,3.0,3.0,3.0,*"
-#outputstring = '$' + outputstring + '*' +  CalcChecksum(outputstring) + '\r\n'
-#ser.write(str.encode(outputstring))
 
 
 
