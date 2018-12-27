1ee338c5065a822a59ffe6e4e887b89bc4c009a8
bnepethomas <bnepethomas@gmail.com>
2018-12-28 06:53:05 +1000

    ready to receive packet

--- Python HW Link/pyHWLink_Serial.py
+++ Python HW Link/pyHWLink_Serial.py
@@ -21,7 +21,203 @@ import time
 import codecs
 import serial
 import string
+import logging
+import os
+import socket
+import sys
+import time
+import threading
 
+logging.basicConfig(format='%(asctime)s:%(levelname)s:%(message)s',level=logging.INFO)
+#logging.basicConfig(format='%(asctime)s:%(levelname)s:%(message)s',level=logging.DEBUG)
+#logging.basicConfig(format='%(asctime)s:%(levelname)s:%(message)s')
+ 
+
+
+# Used for command line parsing
+from optparse import OptionParser
+
+
+def CleanUpAndExit():
+    try:
+        # Catch Ctl-C and quit
+        print('')
+        print('Exiting')
+        print('')
+        try:
+            serverSock.close()
+        except:
+            logging.critical('Unable to close server socket')
+        sys.exit(0)
+
+    except Exception as other:
+        logging.critical('Error in CleanUpAndExit: ' + str(other))
+        sys.exit(0)
+
+
+
+# Global Variables
+debugging = False
+config_file = 'pyHWLink_Serial_config.py'
+secondsBetweenKeepAlives = 5
+
+# Initialise keepalive indicator
+last_time_display = time.time()
+packets_processed = 0
+
+# Address and Port to listen on
+UDP_IP_Address = ""
+UDP_Port = 13136
+
+
+try:
+
+    if not (os.path.isfile(config_file)):
+        
+        logging.info('Unable to find ' + config_file)
+
+        
+    else:
+        try:
+            from config_file import *
+            
+                
+        except Exception as other:
+            logging.critical("Error in Initialisation: " + str(other))
+
+            print('Unable to open "' + config_file + '"' )
+            print('Or variable assignment incorrect - forgot quotes for string?')
+            print('Defaults used')
+
+
+    # The timeout is aggressive
+    serverSock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    serverSock.settimeout(0.0001)
+    serverSock.bind((UDP_IP_Address, UDP_Port))
+
+    wireshark_Sock = socket.socket(socket.AF_INET, # Internet
+        socket.SOCK_DGRAM) # UDP
+
+
+
+
+    # See if value is assigned.  First we checked config file and then
+    #   command line arguments
+
+    logging.debug("Checking Command Line parameters")
+
+
+    parser = OptionParser()
+
+    (options, args) = parser.parse_args()
+
+
+    
+    logging.debug("options:" + str(options))
+    logging.debug("arguments:" +  str(args))
+
+   
+
+except KeyboardInterrupt:
+    CleanUpAndExit()
+
+except Exception as other:
+    logging.critical('Error in Setup: ' + str(other))
+
+
+
+
+def ReceivePacket():
+
+    
+    global last_time_display
+    global packets_processed
+    iterations_Since_Last_Packet = 0
+
+
+    while True:
+        
+        try:
+
+            data, (Source_IP, Source_Port)  = serverSock.recvfrom(1500)
+
+            ReceivedPacket = data
+            packets_processed = packets_processed + 1
+
+            Source_IP = str(Source_IP)
+            Source_Port = str(Source_Port)
+            ReceivedPacket = str(ReceivedPacket)
+            
+            logging.debug("From: " + Source_IP + " " + Source_Port)
+            logging.debug("Message: " + ReceivedPacket)
+
+            ProcessReceivedString( str(ReceivedPacket), Source_IP , str(Source_Port) )
+            
+            logging.debug("Iterations since last packet " + str(iterations_Since_Last_Packet))
+            
+            iterations_Since_Last_Packet=0
+
+                                              
+        except socket.timeout:
+            iterations_Since_Last_Packet = iterations_Since_Last_Packet +  1
+            if debugging == True and (iterations_Since_Last_Packet > 10000):
+                print("[i] Mid Receive Timeout - " + time.asctime())
+                iterations_Since_Last_Packet=0
+                
+            # Throw something on console to show we haven't died
+            if time.time() - last_time_display > secondsBetweenKeepAlives:
+                
+                #Calculate Packets per Second
+                if packets_processed != 0:
+                    packets_per_Second = packets_processed / secondsBetweenKeepAlives
+                else:
+                    packets_per_Second = 0
+                
+                pps_string = ". " + str(packets_per_Second) + " packets per second."
+                
+                logging.info('Keepalive check ' + str(packets_processed)
+                             + ' Packets Processed' + pps_string)
+                
+                last_time_display = time.time()
+                packets_processed = 0
+                
+                
+                                        
+                Send_GPRMC()
+                Send_GPGGA()
+                Send_GPGSA()
+                
+            continue
+
+        except Exception as other:
+            logging.critical('Error in ReceivePacket: ' + str(other))
+            
+
+        if time.time() - last_time_display > 5:
+            print('Keepalive ' + time.asctime())
+            last_time_display = time.time()
+
+
+
+def ProcessReceivedString(ReceivedUDPString, Source_IP, Source_Port):
+
+    logging.debug('Processing UDP String')
+
+    global wireshark_Sock, wireshark_IP_Address, wireshark_Port
+
+    
+    try:
+        if len(ReceivedUDPString) > 0:
+            
+            ReceivedUDPString = str(ReceivedUDPString)
+            logging.debug("From: " + Source_IP + " " + Source_Port)
+            logging.debug('Payload: ' + ReceivedUDPString)
+
+            
+
+    except Exception as other:
+        logging.critical('Error in ProcessReceivedString. Error is: ' + str(other))          
+            
 
 localDebugging = False
 
@@ -115,6 +311,73 @@ def Send_GPGSA():
     ser.write(str.encode(outputstring))
 
 
+
+def Test_Cycle():
+    i = 0
+    j = 0
+
+    global latDegrees
+    global latMinutes 
+    global latSeconds 
+
+    global xoutputval 
+
+    global longDegrees
+    global longMinutes 
+    global longSeconds
+    global outAltitude
+  
+    while ( j < 4600):
+        
+        youtputstr = str(longDegrees).zfill(2) + str(longMinutes).zfill(2) + '.' + str(longSeconds)
+        
+        time.sleep(1)
+        #longMinutes = longMinutes + 1
+        longDegrees = longDegrees + 1
+        if (longMinutes > 59):
+            longMinutes = 0
+            longDegrees = longDegrees + 1
+        if (longDegrees > 179):
+            longDegrees = 0
+                
+        Send_GPRMC()
+        Send_GPGGA()
+        Send_GPGSA()
+            
+
+        j = j + 1
+        i = 0
+        while (i < 100):
+            xoutputval = xoutputval + 1
+            xoutputstr = str(latDegrees).zfill(2) + str(latMinutes).zfill(2) + '.' + str(latSeconds)
+            
+            
+            Send_GPRMC()
+            Send_GPGGA()
+            Send_GPGSA()
+            
+            time.sleep(2)
+            latMinutes = latMinutes + 1
+            #latDegrees = latDegrees + 1
+            if (latMinutes > 59):
+                latMinutes = 0
+                latDegrees = latDegrees + 1
+            if (latDegrees > 74):
+                latDegrees = 0
+             
+             
+             
+            i = i + 1
+            print('Count is ' + str(i) + ' ' + xoutputstr)
+            
+            outAltitude = outAltitude + 10
+            if (outAltitude > 15000):
+                outAltitude = 1
+
+    print('Finished')
+
+
+
 # Reference - teststring
 if False:
     workstring = "GPRMC,160533.00,A,2723.4120,S,15307.72900,E,299,0,010413,0,E"
@@ -146,8 +409,6 @@ Send_GPGSA()
 latDegrees = 27
 latMinutes = 23
 latSeconds = 4120
-i = 0
-j = 0
 
 xoutputval = 2723.4120
 
@@ -156,52 +417,22 @@ longMinutes = 7
 longSeconds = 72900
 
 
-while ( j < 4600):
-    
-    youtputstr = str(longDegrees).zfill(2) + str(longMinutes).zfill(2) + '.' + str(longSeconds)
-    
-    time.sleep(1)
-    #longMinutes = longMinutes + 1
-    longDegrees = longDegrees + 1
-    if (longMinutes > 59):
-        longMinutes = 0
-        longDegrees = longDegrees + 1
-    if (longDegrees > 179):
-        longDegrees = 0
-            
-    Send_GPRMC()
-    Send_GPGGA()
-    Send_GPGSA()
-        
 
-    j = j + 1
-    i = 0
-    while (i < 100):
-        xoutputval = xoutputval + 1
-        xoutputstr = str(latDegrees).zfill(2) + str(latMinutes).zfill(2) + '.' + str(latSeconds)
-        
-        
-        Send_GPRMC()
-        Send_GPGGA()
-        Send_GPGSA()
-        
-        time.sleep(2)
-        latMinutes = latMinutes + 1
-        #latDegrees = latDegrees + 1
-        if (latMinutes > 59):
-            latMinutes = 0
-            latDegrees = latDegrees + 1
-        if (latDegrees > 74):
-            latDegrees = 0
-         
-         
-         
-        i = i + 1
-        print('Count is ' + str(i) + ' ' + xoutputstr)
-        
-        outAltitude = outAltitude + 10
-        if (outAltitude > 15000):
-            outAltitude = 1
 
-print('Finished')
+def Main():
 
+
+
+    print('Listening on port ' + str(UDP_Port))
+    try:
+
+        ReceivePacket()
+
+    except KeyboardInterrupt:
+        # Catch Ctl-C and quit
+        CleanUpAndExit()
+        
+    except Exception as other:
+        logging.critical('Error in Main: ' + str(other))
+        
+Main()        
