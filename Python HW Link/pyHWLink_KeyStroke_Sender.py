e33ed13d982d1ec336e7f11b0dd140cba01ff17f
bnepethomas <bnepethomas@gmail.com>
2019-01-26 09:40:53 +1000

    Update pyHWLink_KeyStroke_Sender.py

--- Python HW Link/pyHWLink_KeyStroke_Sender.py
+++ Python HW Link/pyHWLink_KeyStroke_Sender.py
@@ -158,8 +158,43 @@ def AltTab():
 #    AltTab()
 
 
-KeyStrokeDict = { 'A': [42],
-      'B': [43]}
+KeyStrokeDict = { 'A': [0x41],
+        'B': [0x42],
+        'C': [0x43],
+        'D': [0x44],
+        'E': [0x45],
+        'F': [0x46],
+        'G': [0x47],
+        'H': [0x48],
+        'I': [0x49],
+        'J': [0x4A],
+        'K': [0x4B],
+        'L': [0x4C],
+        'M': [0x4D],
+        'N': [0x4E],
+        'O': [0x4F],
+        'P': [0x50],
+        'Q': [0x51],
+        'R': [0x52],
+        'S': [0x53],
+        'T': [0x54],
+        'U': [0x55],
+        'V': [0x56],
+        'W': [0x57],
+        'X': [0x58],
+        'Y': [0x59],
+        'Z': [0x5A],
+
+        '0': [0x30],
+        '1': [0x31],
+        '2': [0x32],
+        '3': [0x33],
+        '4': [0x34],
+        '5': [0x35],
+        '6': [0x36],
+        '7': [0x37],
+        '8': [0x38],
+        '9': [0x39] }
 
 
 
@@ -258,17 +293,21 @@ def ProcessReceivedString(ReceivedUDPString):
                     CommandsToProcess.remove(ModifierToCheck)
                     ModifierSet.insert(0,ModifierToCheck)
 
-
-##                if 'Alt' in CommandsToProcess:
-##                    print('Found an Alt')
-##                    # Remove it from the set and add to ModifierSet
-##                    CommandsToProcess.remove('Alt')
-##                    ModifierSet.insert(0,'Alt')
-                    
-            print( CommandsToProcess)
+            # Now have twos sets, one contains strings to send and the other modifers
             
-                    
+            # In test mode so don't send modifers just yet
+            
+            print( CommandsToProcess)
 
+            # Originally was supporting a text string without spaces
+            # But that makes it difficult to deal with F1 ESC etc
+            
+            for CommandToSend in CommandsToProcess:
+                print( KeyStrokeDict.get(CommandToSend.upper()))
+                if KeyStrokeDict.get(CommandToSend.upper()) != None:
+                    PressKey( int(KeyStrokeDict.get(CommandToSend.upper())[0]))
+                    time.sleep(0.02)
+                    ReleaseKey(int(KeyStrokeDict.get(CommandToSend.upper())[0]))
  
     except Exception as other:
         logging.critical("Error in ProcessReceivedString: " + str(other))
