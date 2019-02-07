942dbb07b8c35de12150db7ecca247c23ba30d0a
bnepethomas <pethomas@cisco.com>
2019-02-07 15:58:01 +1000

    images

--- Python HW Link/test_py_image.py
+++ Python HW Link/test_py_image.py
@@ -14,26 +14,31 @@ class SimpleApp(object):
         self.canvas.pack()
 
         self.update = self.draw().__next__
-        master.after(100, self.update)
+        master.after(2000, self.update)
 
     def draw(self):
         image = Image.open(self.filename)
+        image2 = Image.open("HSI Outline.png")
 
         angle = 0
         while True:
-            canvas_obj = self.canvas.create_rectangle(0,0,20,20)
-          
-            tkimage = ImageTk.PhotoImage(image.rotate(angle))
-            canvas_obj = self.canvas.create_image(500, 500, image=tkimage)
+            #canvas_obj = self.canvas.create_rectangle(0,0,20,20,fill='red')
 
-            canvas_obj = self.canvas.create_oval(100,100,400,400)
+            
+            tkimage = ImageTk.PhotoImage(image.rotate(angle))
+            tkimage2 = ImageTk.PhotoImage(image2)
+            canvas_obj = self.canvas.create_image(500, 500, image=tkimage)
+            canvas_obj = self.canvas.create_image(500,500, image=tkimage2)
+            canvas_obj = self.canvas.create_oval(500,500,550,550,fill='yellow')
+          
+            # canvas_obj = self.canvas.create_oval(100,100,400,400,fill='green')
 
             self.master.after_idle(self.update)
             
             yield
-            time.sleep(0.1)
+
             self.canvas.delete(canvas_obj)
-            angle += 1
+            angle += 5
             angle %= 360
 
 root = tk.Tk()
