ef64768a1b16bc480ed1ee54d9b65ce648925e97
bnepethomas <bnepethomas@gmail.com>
2019-05-26 06:45:33 +1000

    Doc update

--- Python HW Link/input_config.py
+++ Python HW Link/input_config.py
@@ -1,3 +1,7 @@
+# Both FlightSim and Aircraft type are important
+# as they determine which methods of communication is used
+# Currently this is largely due to XPlane using packed strings
+# versus the free text used by DCS
 learning = False
 AircraftType = 'A10C'
 FlightSim = 'DCS'
