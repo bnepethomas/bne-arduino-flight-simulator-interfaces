de908f97f3056765a8db0e3a614839882a7d3270
bnepethomas <bnepethomas@gmail.com>
2019-05-31 13:48:51 +1000

    work

--- Control_P3d/Managed Data Request/Data Request.cs
+++ Control_P3d/Managed Data Request/Data Request.cs
@@ -34,6 +34,9 @@ namespace Managed_Data_Request
     public partial class Form1 : Form
     {
 
+
+        static public string mystring;
+
         // User-defined win32 event
         const int WM_USER_SIMCONNECT = 0x0402;
 
@@ -96,9 +99,6 @@ namespace Managed_Data_Request
         {
             public double hdg;
         };
-
-
-
 
 
 
@@ -355,6 +355,58 @@ namespace Managed_Data_Request
         }
     }
 
+    public class UDPSocket
+    {
+        private Socket _socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+        private const int bufSize = 8 * 1024;
+        private State state = new State();
+        private EndPoint epFrom = new IPEndPoint(IPAddress.Any, 0);
+        private AsyncCallback recv = null;
+
+        public class State
+        {
+            public byte[] buffer = new byte[bufSize];
+        }
+
+        public void Server(string address, int port)
+        {
+            _socket.SetSocketOption(SocketOptionLevel.IP, SocketOptionName.ReuseAddress, true);
+            _socket.Bind(new IPEndPoint(IPAddress.Parse(address), port));
+            Receive();
+        }
+
+        public void Client(string address, int port)
+        {
+            _socket.Connect(IPAddress.Parse(address), port);
+            Receive();
+        }
+
+        public void Send(string text)
+        {
+            byte[] data = Encoding.ASCII.GetBytes(text);
+            _socket.BeginSend(data, 0, data.Length, SocketFlags.None, (ar) =>
+            {
+                State so = (State)ar.AsyncState;
+                int bytes = _socket.EndSend(ar);
+                Console.WriteLine("SEND: {0}, {1}", bytes, text);
+
+            }, state);
+        }
+
+        private void Receive()
+        {
+            _socket.BeginReceiveFrom(state.buffer, 0, bufSize, SocketFlags.None, ref epFrom, recv = (ar) =>
+            {
+                State so = (State)ar.AsyncState;
+                int bytes = _socket.EndReceiveFrom(ar, ref epFrom);
+                _socket.BeginReceiveFrom(so.buffer, 0, bufSize, SocketFlags.None, ref epFrom, recv, so);
+                Console.WriteLine("RECV: {0}: {1}, {2}", epFrom.ToString(), bytes, Encoding.ASCII.GetString(so.buffer, 0, bytes));
+                Form1.mystring = Encoding.ASCII.GetString(so.buffer, 0, bytes);
+
+            }, state);
+        }
+    }
+
+
 }
 // End of sample
-                
