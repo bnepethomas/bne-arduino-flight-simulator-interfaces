a63e45727bd1224dbbf84ace8e3e84d0112dd62c
bnepethomas <pethomas@cisco.com>
2019-06-22 12:00:09 +1000

    Update UDP_Gauge_Test_Sender.py

--- Python HW Link/UDP_Gauge_Test_Sender.py
+++ Python HW Link/UDP_Gauge_Test_Sender.py
@@ -61,69 +61,6 @@ def Translate_Value(value_to_process):
     
     # Have integer - no do range check
 
-    speeds = [260,250,240,230,220,210,200,190,180,170,160,150,140,130,120,110,100,90 ,80 ,70 ,60 ,50 ,40 ,30 ,0]
-    pos    = [29 ,35 ,40 ,45 ,50 ,55 ,60 ,67 ,72 ,78 ,86 ,95 ,102,109,118,125,131,143,150,157,161,171,173,180,180]
-
-
-    
-    if (len(speeds) != len(pos)):
-        logging.critical("Lookup arrays have different lengths. speed has " + str(len(speeds)) + \
-                   " pos has " + str(len(pos)))
-
-    for i in range( len(speeds)):
-        print(str(i) + " " + str(speeds[i]) + " " + str(pos[i]))
-    
-    if (local_value >= 260):
-        value_to_process = 29
-    elif (local_value >= 250):
-        value_to_process = 35
-    elif (local_value >= 240):
-        value_to_process = 40
-    elif (local_value >= 230):
-        value_to_process = 45
-    elif (local_value >= 220):
-        value_to_process = 50        
-    elif (local_value >= 210):
-        value_to_process = 55
-    elif (local_value >= 200):
-        value_to_process = 60
-    elif (local_value >= 190):
-        value_to_process = 67
-    elif (local_value >= 180):
-        value_to_process = 72
-    elif (local_value >= 170):
-        value_to_process = 78
-    elif (local_value >= 160):
-        value_to_process = 86
-    elif (local_value >= 150):
-        value_to_process = 95        
-    elif (local_value >= 140):
-        value_to_process = 102 
-    elif (local_value >= 130):
-        value_to_process = 109
-    elif (local_value >= 120):
-        value_to_process = 118
-    elif (local_value >= 110):
-        value_to_process = 125
-    elif (local_value >= 100):
-        value_to_process = 131
-    elif (local_value >= 90):
-        value_to_process = 143
-    elif (local_value >= 80):
-        value_to_process = 150
-    elif (local_value >= 70):
-        value_to_process = 157        
-    elif (local_value >= 60):
-        value_to_process = 161
-    elif (local_value >= 50):
-        value_to_process = 171
-    elif (local_value >= 40):
-        value_to_process = 173        
-    elif (local_value >= 30):
-        value_to_process = 180      
-    elif (local_value <= 0):
-        value_to_process = 180
-
 # 260 29
 #250 35
 #240 40
@@ -149,6 +86,49 @@ def Translate_Value(value_to_process):
 #40 173
 #30 180
 #20 180
+
+    speeds = [260,250,240,230,220,210,200,190,180,170,160,150,140,130,120,110,100,90 ,80 ,70 ,60 ,50 ,40 ,30 ,0]
+    pos    = [29 ,35 ,40 ,45 ,50 ,55 ,60 ,67 ,72 ,78 ,86 ,95 ,102,109,118,125,131,143,150,157,161,171,173,180,180]
+
+
+    
+    if (len(speeds) != len(pos)):
+        logging.critical("Lookup arrays have different lengths. speed has " + str(len(speeds)) + \
+                   " pos has " + str(len(pos)))
+        
+
+
+    if (local_value > speeds[0]):
+        # Nice easy one already at max, so just return that
+        value_to_process = pos[0]
+    else:
+        try:
+            for i in range( len(speeds)):
+                #print(str(i) + " " + str(speeds[i]) + " " + str(pos[i]))
+                if (local_value >= speeds[i]):
+                    value_to_process = pos[i]
+
+                    # Calc difference between and last step
+                    # Note values in array must start at max and decrement to 0
+                    # Determine if incremental calculation is needed
+                    if (local_value != speeds[i]):
+                        input_difference = (local_value - speeds[i]) * 100/(speeds[i-1] - speeds[i])
+                        output_difference = pos[i] - pos[i-1]
+
+                        positional_change = int(input_difference / output_difference)
+                        
+                        print ("Input Difference is " + str(input_difference))
+                        print ("Output Difference is " + str(output_difference))                        
+                        print("Positional change is " + str(positional_change))
+
+                        value_to_process = value_to_process - positional_change
+                    break
+        except Exception as other:
+            logging.critical('[e] Error in Translate_Value: ' + str(other))
+            return('180')
+
+
+
 
 
     logging.info('value_to_process at end of translate is :' + str(value_to_process))
