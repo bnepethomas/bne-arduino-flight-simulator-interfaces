86066d3ea3e867b217babf85c09c93e9a9c5340f
bnepethomas <pethomas@cisco.com>
2019-06-22 10:18:59 +1000

    Update UDP_Gauge_Test_Sender.py

--- Python HW Link/UDP_Gauge_Test_Sender.py
+++ Python HW Link/UDP_Gauge_Test_Sender.py
@@ -47,17 +47,100 @@ sock.bind((UDP_IP, UDP_PORT))
 
 def Translate_Value(value_to_process):
 
+    local_value = 0
     
-    logging.critical('value_to_process at start of translate is :' + value_to_process)
-    
-    if (value_to_process == '0'):
-        value_to_process = '180'
-    elif (value_to_process == '260'):
-        value_to_process = '0'
+    logging.info('value_to_process at start of translate is :' + value_to_process)
 
-   
-    logging.critical('value_to_process at end of translate is :' + value_to_process)
-    return(value_to_process)
+    # Convert to int for airspeed
+    try:
+        local_value = int(value_to_process)
+    except ValueError:
+        logging.critical("None numeric value passed to Translate Value. Value is :" + value_to_process)
+        #Center pointer - hopefully lowest risk
+        return('180')
+    
+    # Have integer - no do range check
+    
+    if (local_value >= 260):
+        value_to_process = 29
+    elif (local_value >= 250):
+        value_to_process = 35
+    elif (local_value >= 240):
+        value_to_process = 40
+    elif (local_value >= 230):
+        value_to_process = 45
+    elif (local_value >= 220):
+        value_to_process = 50        
+    elif (local_value >= 210):
+        value_to_process = 55
+    elif (local_value >= 200):
+        value_to_process = 60
+    elif (local_value >= 190):
+        value_to_process = 67
+    elif (local_value >= 180):
+        value_to_process = 72
+    elif (local_value >= 170):
+        value_to_process = 78
+    elif (local_value >= 160):
+        value_to_process = 86
+    elif (local_value >= 150):
+        value_to_process = 95        
+    elif (local_value >= 140):
+        value_to_process = 102 
+    elif (local_value >= 130):
+        value_to_process = 109
+    elif (local_value >= 120):
+        value_to_process = 118
+    elif (local_value >= 110):
+        value_to_process = 125
+    elif (local_value >= 100):
+        value_to_process = 131
+    elif (local_value >= 90):
+        value_to_process = 143
+    elif (local_value >= 80):
+        value_to_process = 150
+    elif (local_value >= 70):
+        value_to_process = 157        
+    elif (local_value >= 60):
+        value_to_process = 161
+    elif (local_value >= 50):
+        value_to_process = 171
+    elif (local_value >= 40):
+        value_to_process = 173        
+    elif (local_value >= 30):
+        value_to_process = 180      
+    elif (local_value <= 0):
+        value_to_process = 180
+
+# 260 29
+#250 35
+#240 40
+#230 45
+#220 50
+#210 55
+#200 60
+#190 67
+#180 72
+#170 78 
+#160 86
+#150 95
+#140 102
+#130 109
+#120 118
+#110 125
+#100 131
+#90 143
+#80 150
+#70 157
+#60 161
+#50 171
+#40 173
+#30 180
+#20 180
+
+
+    logging.info('value_to_process at end of translate is :' + str(value_to_process))
+    return(str(value_to_process))
 
 
 
