0f0f1775fdd2ef760f8085b4eed3ea952503b440
bnepethomas <peterthomas@Peters-Mac-mini-2165.local>
2020-06-06 13:48:26 +1000

    Update Hornet_UFC.ino

--- Hornet UFC/Hornet_UFC/Hornet_UFC.ino
+++ Hornet UFC/Hornet_UFC/Hornet_UFC.ino
@@ -6,10 +6,13 @@
 // displays at the bottom left and bottom right hand side.
 //
 // As a number of the smae OLEDs are used, which the same target I2C addresses an I2C multiplexor is used, TCA9548A.
-// The TCA9548A is an 8 Channel I2C switch.  It is possible for different devices to share a common bus.
+// The TCA9548A is an 8 Channel I2C switch.  It is possible for different devices to share a common host I2C bus.
 // https://e2e.ti.com/blogs_/b/analogwire/archive/2015/10/15/how-to-simplify-i2c-tree-when-connecting-multiple-slaves-to-an-i2c-master
 //
 // Prototyping is being done with an Adafruit 2717 TCA9548A board.
+// The 8 channels have their own SCL and SDA
+// Had one weirdness I'm yet to understand - the SCL SDA appear to be reversed on the Adafruit outputs, the input pins
+// is as epxected, but the outputs appeared to be swapped around.
 
 // The initial test OLEDs have addresses of 0x3C - despite being labelled - 0x78 or 0x7A - selectable by soldering a jumper. I'm assuming these are the same SSD1306 
 // used in the Hornet Altimeter.
@@ -28,13 +31,134 @@
 // I2C 0x3C - validate by running scanner
 
 
+// Basic logic
+// Initialise I2C Bus (wire)
+// Iterate through each port on the Mux and list connected devices to serial port (simple troubleshooting aid), may use UDP later
+// Initialise each display
+// Wait for UDP updates
+
+
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+// Unsure what this does - hopefully not a pin on the mega
+#define OLED_RESET 4
+Adafruit_SSD1306 display(OLED_RESET);
+
+#define NUMFLAKES 10
+#define XPOS 0
+#define YPOS 1
+#define DELTAY 2
+
+
+#define LOGO16_GLCD_HEIGHT 16 
+#define LOGO16_GLCD_WIDTH  16 
+static const unsigned char PROGMEM logo16_glcd_bmp[] =
+{ B00000000, B11000000,
+  B00000001, B11000000,
+  B00000001, B11000000,
+  B00000011, B11100000,
+  B11110011, B11100000,
+  B11111110, B11111000,
+  B01111110, B11111111,
+  B00110011, B10011111,
+  B00011111, B11111100,
+  B00001101, B01110000,
+  B00011011, B10100000,
+  B00111111, B11100000,
+  B00111111, B11110000,
+  B01111100, B11110000,
+  B01110000, B01110000,
+  B00000000, B00110000 };
+
+#if (SSD1306_LCDHEIGHT != 32)
+#error("Height incorrect, please fix Adafruit_SSD1306.h!");
+#endif
+
+extern "C" { 
+#include "utility/twi.h"  // from Wire library, so we can do bus scanning
+}
+
+#define TCAADDR 0x70
+
+int CurrentDisplay = 3;
+
+ 
+void tcaselect(uint8_t i) {
+  if (i > 7) return;
+ 
+  Wire.beginTransmission(TCAADDR);
+  Wire.write(1 << i);
+  Wire.endTransmission();  
+}
 
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(115200);
+  Wire.begin();
+
+  Serial.println("\nScanning I2C Bus");
+  
+  for (uint8_t t=0; t<8; t++) {
+    tcaselect(t);
+    Serial.print("TCA Port #"); Serial.println(t);
+
+    for (uint8_t addr = 0; addr<=127; addr++) {
+      if (addr == TCAADDR) continue;
+    
+      uint8_t data;
+      if (! twi_writeTo(addr, &data, 0, 1, 1)) {
+         Serial.print("Found I2C 0x");  Serial.println(addr,HEX);
+      }
+    }
+  }
+  Serial.println("\nI2C scan complete"); 
 
 }
 
+
+
+
 void loop() {
-  // put your main code here, to run repeatedly:
+
+  if (CurrentDisplay == 2) {
+    CurrentDisplay = 3;
+  } else
+  {
+    CurrentDisplay = 2;
+  }
+
+  tcaselect(CurrentDisplay);
+
+
+  // by default, we'll generate the high voltage from the 3.3v line internally! (neat!)
+  display.begin(SSD1306_SWITCHCAPVCC, 0x3C);  // initialize with the I2C addr 0x3C (for the 128x32)
+  // init done
+  
+  // Show image buffer on the display hardware.
+  // Since the buffer is intialized with an Adafruit splashscreen
+  // internally, this will display the splashscreen.
+  display.display();
+  delay(0);
+
+  // Clear the buffer.
+  display.clearDisplay();
+
+    // text display tests
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  display.setCursor(0,0);
+  display.println("Hello, world!");
+  display.setTextColor(BLACK, WHITE); // 'inverted' text
+  display.println(3.141592);
+  display.setTextSize(2);
+  display.setTextColor(WHITE);
+  display.print("0x"); display.println(0xDEADBEEF, HEX);
+  display.display();
+  delay(2000);
+  display.clearDisplay();
+
+  
 
 }
