0389655c360e6f313c7b60dff53f29370222fe32
bnepethomas <peterthomas@Peters-Mac-mini-2165.local>
2020-06-27 14:31:05 +1000

    Test functioning with 14 Seg Font

--- Hornet UFC/Hornet_UFC/Hornet_UFC.ino
+++ Hornet UFC/Hornet_UFC/Hornet_UFC.ino
@@ -2,6 +2,9 @@
 //
 // The project intends to drive the OLED displays on a F18C Hornet Up Front Controller
 //
+// To do  - grab Analog 0 use that to locally to determine brightness - no real value in getting that from the Sim
+//        - develop font based on 14 segment display
+//
 // The UFC has a large display on the top left hand corner, five mid sized display on the right hand side, and then two smaller 
 // displays at the bottom left and bottom right hand side.
 //
@@ -16,7 +19,7 @@
 
 // The initial test OLEDs have addresses of 0x3C - despite being labelled - 0x78 or 0x7A - selectable by soldering a jumper. I'm assuming these are the same SSD1306 
 // used in the Hornet Altimeter.
-// Can validate wha addresses are on the bus by using I2C scanner
+// Can validate what addresses are on the bus by using I2C scanner
 
 // The following OLEDs where sourced from ebay
 // OLED for top  2.2" 128 * 32 SSD1305 (not 1306)
@@ -30,6 +33,7 @@
 // 8 & 9 SDA
 // 16 Reset
 
+// Useful reference for troubleshooting OLEDs
 
 // OLED for 5 Right hand side digits 0.91" 128*32 SSD1306
 // https://www.ebay.com/itm/0-91-Inch-128x32-IIC-I2c-White-Blue-OLED-LCD-Display-Module-3-3-5v-For-Arduino/392552169768?ssPageName=STRK%3AMEBIDX%3AIT&var=661536491479&_trksid=p2057872.m2749.l2649
@@ -46,15 +50,18 @@
 // Iterate through each port on the Mux and list connected devices to serial port (simple troubleshooting aid), may use UDP later
 // Initialise each display
 // Wait for UDP updates
+// during timeout grab and smooth analo 0 and get brightness for all displays
 
 
 #include <SPI.h>
 #include <Wire.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
+#include <Fonts/FreeMonoBoldOblique24pt7b.h>
 #include <Fonts/FreeMonoBoldOblique12pt7b.h>
 #include <Fonts/FreeMonoBold18pt7b.h>  
 #include <Fonts/FreeMono9pt7b.h>   
+#include "PTFont.h"
 #include "er_oled.h"
 
 // Unsure what this does - hopefully not a pin on the mega
@@ -73,6 +80,7 @@ extern "C" {
 
 uint8_t oled_buf[WIDTH * HEIGHT / 8];
 int CurrentDisplay = 0;
+int Brightness = 0;
 char buffer[20]; //plenty of space for the value of millis() plus a zero terminator
  
 void tcaselect(uint8_t i) {
@@ -115,12 +123,30 @@ void setup() {
 
 }
 
-
+void setContrast(int contr){
+    //From https://www.youtube.com/watch?v=hFpXfSnDNSY
+    //
+    // for Adafruit_SSD1306.h library
+    // contr values from 1 to 411
+    // for i2c and 3.3V VCC works fine
+    int prech;
+    int brigh; 
+    switch (contr){
+      case 001 ... 255: prech= 0; brigh= contr; break;
+      case 256 ... 411: prech=16; brigh= contr-156; break;
+      default: prech= 16; brigh= 255; break;}
+      
+    display.ssd1306_command(SSD1306_SETPRECHARGE);      
+    display.ssd1306_command(prech);                            
+    display.ssd1306_command(SSD1306_SETCONTRAST);         
+    display.ssd1306_command(brigh);                           
+}
 
 
 void loop() {
 
 
+    
   CurrentDisplay ++;
   if (CurrentDisplay >7) 
     CurrentDisplay = 0;
@@ -131,7 +157,9 @@ void loop() {
   {
     er_oled_clear(oled_buf);
     sprintf(buffer, "%d", millis());  //convert millis() to a char array in buffer and terminate it with a zero
+    
     er_oled_string(40, 7, buffer, 16, 1, oled_buf);  
+    
     er_oled_display(oled_buf);
   } else
   {
@@ -145,12 +173,26 @@ void loop() {
     display.setCursor(0,10);
   
     display.println(CurrentDisplay);
-
-    display.setFont(&FreeMonoBold18pt7b);
-    display.setCursor(10,30);
+    //display.setFont(&FreeMonoBoldOblique24pt7b);
+    display.setFont(&DSEG14_Classic_Regular_33);
+    //display.setFont(&FreeMonoBold18pt7b);
+    display.setCursor(10,32);
     display.println(millis());
-    
+
+    randomSeed(analogRead(0));
+    Brightness = random(254);
+    if (Brightness > 120)
+      setContrast(411); 
+    else
+      setContrast(1); 
     display.display();
+
+    
+
+
+
+    
+    
 
   }
   
