709b90b12d5be5a3aebd245a1fd1972d4695d04b
bnepethomas <peterthomas@Peters-Mac-mini-2165.local>
2020-06-29 06:09:00 +1000

    Update u8g2scatch.ino

--- u8g2ScratchArea/u8g2scatch/u8g2scatch.ino
+++ u8g2ScratchArea/u8g2scatch/u8g2scatch.ino
@@ -6,9 +6,12 @@
 //U8G2_SSD1306_128X32_UNIVISION_1_SW_I2C u8g2(U8G2_R0, /* clock=*/ SCL, /* data=*/ SDA, /* reset=*/ U8X8_PIN_NONE); 
 U8G2_SSD1306_64X48_ER_1_HW_I2C u8g2(U8G2_R0, /* reset=*/ 4); 
 
+U8G2_SSD1306_64X48_ER_1_HW_I2C u8g2a(U8G2_R0, /* reset=*/ 4); 
+
 /* u8g2.begin() is required and will sent the setup/init sequence to the display */
 void setup(void) {
   u8g2.begin();
+  u8g2a.begin();
 }
 
 /* draw something on the display with the `firstPage()`/`nextPage()` loop*/
@@ -36,15 +39,15 @@ void loop(void) {
     delay(10);
     
   }
-  u8g2.firstPage();
+  u8g2a.firstPage();
   do {
  
-    u8g2.setFont(u8g2_font_fub35_tr);
-    u8g2.setCursor(xpos2,ypos1);
-    u8g2.print("C");
+    u8g2a.setFont(u8g2_font_fub35_tr);
+    u8g2a.setCursor(xpos2,ypos1);
+    u8g2a.print("C");
 
     
-  } while ( u8g2.nextPage() );
+  } while ( u8g2a.nextPage() );
   delay(40);
 
 }
