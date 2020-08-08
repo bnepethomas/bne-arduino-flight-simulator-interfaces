c7d9bd25c0f940dad744e7e7df13a4394dfd9f5a
bnepethomas <pethomas@cisco.com>
2020-08-08 12:58:43 +1000

    WORK

--- DCS BIOS Sketches/DCSBIOS_Input_Controller/DCSBIOS_Input_Controller.ino
+++ DCS BIOS Sketches/DCSBIOS_Input_Controller/DCSBIOS_Input_Controller.ino
@@ -234,6 +234,18 @@ void SendDCSBIOSMessage(int ind, int state) {
         case 3:
           sendDcsBiosMessage("LEFT_DDI_PB_10", "0");
           break; 
+        case 4:
+          break;
+        case 5:
+          break;
+        case 7:
+          break;             
+        case 8:
+          break;
+        case 9:
+          break;
+        case 10:
+          break;
         case 11:
           sendDcsBiosMessage("LEFT_DDI_PB_04", "0");
           break;             
@@ -246,6 +258,18 @@ void SendDCSBIOSMessage(int ind, int state) {
         case 14:
           sendDcsBiosMessage("LEFT_DDI_PB_09", "0");
           break; 
+        case 15:
+          break;
+        case 17:
+          break;             
+        case 18:
+          break;
+        case 19:
+          break;
+        case 20:
+          break;
+        case 21:
+          break;
         case 22:
           sendDcsBiosMessage("LEFT_DDI_PB_03", "0");
           break;
@@ -300,6 +324,8 @@ void SendDCSBIOSMessage(int ind, int state) {
           break;             
         case 3:
           sendDcsBiosMessage("LEFT_DDI_PB_10", "1");
+          break; 
+        case 4:
           break; 
         case 11:
           sendDcsBiosMessage("LEFT_DDI_PB_04", "1");
