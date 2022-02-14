b2be6ad6e7439050088f2fc3ce4396161c8b4f5f
bnepethomas <bnepethomas@gmail.com>
2022-02-14 17:01:29 +1000

    working

--- DCS BIOS Sketches/Getting Close to Completion/CURRENT_CODES-BEN/FRONT_OUTPUT_FP/FRONT_OUTPUT_FP.ino
+++ DCS BIOS Sketches/Getting Close to Completion/CURRENT_CODES-BEN/FRONT_OUTPUT_FP/FRONT_OUTPUT_FP.ino
@@ -61,11 +61,18 @@ char trimpacketBuffer[1000];           //buffer to store trim data
 unsigned long NEXT_STATUS_TOGGLE_TIMER = 0;
 bool GREEN_LED_STATE = false;
 
+
+#define LAUNCH_BAR_PORT 27
+#define HOOK_BYPASS_PORT 28
+#define FUEL_DUMP_PORT 29
 #define APU_PORT 30
 #define ENGINE_CRANK_PORT 31
-#define FUEL_DUMP_PORT 29
-#define HOOK_BYPASS_PORT 28
-#define LAUNCH_BAR_PORT 27
+
+
+#define BLEED_AIR_SOL_PORT 22
+#define PITOT_HEAT_PORT 23
+#define LASER_ARM_PORT 24
+#define CANOPY_MAG_PORT 25
 
 unsigned long NEXT_PORT_TOGGLE_TIMER = 0;
 bool PORT_OUTPUT_STATE = false;
@@ -224,12 +231,27 @@ void setup() {
   digitalWrite( GREEN_STATUS_LED_PORT, false);
 
   delay(FLASH_TIME);
-
   pinMode( APU_PORT,  OUTPUT);
   pinMode( ENGINE_CRANK_PORT,  OUTPUT);
   pinMode( FUEL_DUMP_PORT,  OUTPUT);
   pinMode( HOOK_BYPASS_PORT,  OUTPUT);
   pinMode( LAUNCH_BAR_PORT,  OUTPUT);
+  pinMode( BLEED_AIR_SOL_PORT,  OUTPUT);
+  pinMode( PITOT_HEAT_PORT,  OUTPUT);
+  pinMode( LASER_ARM_PORT,  OUTPUT);
+  pinMode( CANOPY_MAG_PORT,  OUTPUT);
+
+
+  // Turn everything off
+  digitalWrite(APU_PORT, false);
+  digitalWrite(ENGINE_CRANK_PORT, false);
+  digitalWrite(FUEL_DUMP_PORT, false);
+  digitalWrite(HOOK_BYPASS_PORT, false);
+  digitalWrite(LAUNCH_BAR_PORT, false);
+  digitalWrite(BLEED_AIR_SOL_PORT, false);
+  digitalWrite(PITOT_HEAT_PORT, false);
+  digitalWrite(LASER_ARM_PORT, false);
+  digitalWrite(CANOPY_MAG_PORT, false);
 
 
   // Turn everything off
@@ -243,36 +265,34 @@ void setup() {
 
 
   RAD_ALT_servo.attach(RadarAltServoPin);
-  RAD_ALT_servo.writeMicroseconds(1420);  // set servo to "Off Point"
-  delay(300);
-  RAD_ALT_servo.detach();
-
   HYD_LFT_servo.attach(HydLeftServoPin);
-  HYD_LFT_servo.writeMicroseconds(1100);  // set servo to "Mid Point"
-  delay(300);
-  HYD_LFT_servo.detach();
-
   HYD_RHT_servo.attach(HydRightServoPin);
-  HYD_RHT_servo.writeMicroseconds(1100);  // set servo to "Mid Point"
-  delay(300);
-  HYD_RHT_servo.detach();
-
   BATT_U_servo.attach(VoltUServoPin);
-  BATT_U_servo.writeMicroseconds(1100);  // set servo to "Mid Point"
-  delay(300);
-  BATT_U_servo.detach();
-
   BATT_E_servo.attach(VoltEServoPin);
-  BATT_E_servo.writeMicroseconds(1100);  // set servo to "Mid Point"
-  delay(300);
-  BATT_E_servo.detach();
-
-
   TRIM_servo.attach(TrimServoPin);
+
+  RAD_ALT_servo.writeMicroseconds(1420);  // set servo to "Off Point"
+  HYD_LFT_servo.writeMicroseconds(1100);  // set servo to "Mid Point"
+  HYD_RHT_servo.writeMicroseconds(1100);  // set servo to "Mid Point"
+  BATT_U_servo.writeMicroseconds(1100);  // set servo to "Mid Point"
+  BATT_E_servo.writeMicroseconds(1100);  // set servo to "Mid Point"
   TRIM_servo.writeMicroseconds(1100);  // set servo to "Mid Point"
   delay(1000);
-  TRIM_servo.writeMicroseconds(500);  // set servo to "Mid Point"
-  delay(400);
+
+  HYD_LFT_servo.writeMicroseconds(800);  // set servo to "Mid Point"
+  HYD_RHT_servo.writeMicroseconds(800);  // set servo to "Mid Point"
+  BATT_U_servo.writeMicroseconds(800);  // set servo to "Mid Point"
+  BATT_E_servo.writeMicroseconds(800);  // set servo to "Mid Point"
+  TRIM_servo.writeMicroseconds(800);  // set servo to "Mid Point"
+
+  delay(500);
+
+
+  RAD_ALT_servo.detach();
+  HYD_RHT_servo.detach();
+  HYD_LFT_servo.detach();
+  BATT_U_servo.detach();
+  BATT_E_servo.detach();
   TRIM_servo.detach();
 
 
@@ -289,7 +309,7 @@ void setup() {
   stepperCA.setSpeed(60);
   stepperCA.step(700);       //Reset FULL ON Position
   stepperCA.step(-720);       //Reset FULL OFF Position
-  stepperCA.step(30);       //Reset FULL OFF Position  
+  stepperCA.step(30);       //Reset FULL OFF Position
   posCA = 0;
   CAB_ALT = map(0, 0, 65000, 40, 720);
   /// CABIN ALT WORKING ======< SET CABIN ALT STEPPER TO 0 FEET
