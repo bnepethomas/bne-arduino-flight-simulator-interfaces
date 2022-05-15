ed3c9acc59cecbc54497ed5ab0e93c4c149b3a27
bnepethomas <bnepethomas@gmail.com>
2022-05-15 10:21:37 +1000

    Update pyHWLink_SimConnect.py

--- Python HW Link/pyHWLink_SimConnect.py
+++ Python HW Link/pyHWLink_SimConnect.py
@@ -3,9 +3,11 @@ from SimConnect import *
 # Create SimConnect link
 try: 
     sm = SimConnect()
-        
+    print("Sim Connected")    
     # Note the default _time is 2000 to be refreshed every 2 seconds
+    print("Asking for aircraft info")
     aq = AircraftRequests(sm, _time=2000)
+    print("Configured Aircraft Requests")
     # Use _time=ms where ms is the time in milliseconds to cache the data.
     # Setting ms to 0 will disable data caching and always pull new data from the sim.
     # There is still a timeout of 4 tries with a 10ms delay between checks.
@@ -15,7 +17,10 @@ try:
     # To find and set timeout of cached data to 200ms:
     altitude = aq.find("PLANE_ALTITUDE")
     altitude.time = 200
+    print("Grabbing Altitude")
    
-except:
-    print("Sim not running?")
+except Exception as error:
+   error_string = str(error)
+   print(error_string)
+   print("Sim not running?")
 
