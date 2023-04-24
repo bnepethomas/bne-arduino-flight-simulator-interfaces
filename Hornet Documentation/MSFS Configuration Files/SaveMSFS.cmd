f18e37151bec62cf8875f22d4358f5a49f3d5281
bnepethomas <bnepethomas@gmail.com>
2023-04-24 10:23:24 +1000

    views 20230423

---
+++ Hornet Documentation/MSFS Configuration Files/SaveMSFS.cmd
@@ -0,0 +1,5 @@
+copy C:\Users\bnepe\AppData\Local\Packages\Microsoft.FlightSimulator_8wekyb3d8bbwe\LocalCache\Packages\Official\OneStore\microsoft-aircraft-bell407\SimObjects\Airplanes\microsoft-aircraft-bell407\model\model.CFG c:\xfer\407
+copy C:\Users\bnepe\AppData\Local\Packages\Microsoft.FlightSimulator_8wekyb3d8bbwe\LocalCache\Packages\Official\OneStore\microsoft-aircraft-bell407\SimObjects\Airplanes\microsoft-aircraft-bell407\cameras.cfg c:\xfer\407
+copy C:\Users\bnepe\AppData\Local\Packages\Microsoft.FlightSimulator_8wekyb3d8bbwe\LocalCache\UserCfg.opt c:\xfer
+xcopy c:\xfer . /s
+pause
