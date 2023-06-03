b71fdff71241834cab3d81e2677d10fddf4692fb
bnepethomas <peterthomas@Peters-Mac-mini-2765.local>
2023-06-03 12:20:31 +1000

    323

--- DCS BIOS Sketches/In Development/Ethernet_Test/Ethernet_Test.ino
+++ DCS BIOS Sketches/In Development/Ethernet_Test/Ethernet_Test.ino
@@ -2,24 +2,30 @@
 #include <Ethernet.h>
 
 byte mac[] = {  0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
-IPAddress ip(172,16,1,11);
+IPAddress ip(172, 16, 1, 11);
 
-#define RED_STATUS_LED_PORT 13               // RED LED is used for monitoring ethernet
-#define GREEN_STATUS_LED_PORT 5               // RED LED is used for monitoring ethernet
+#define RED_STATUS_LED_PORT 5               // RED LED is used for monitoring ethernet
+#define GREEN_STATUS_LED_PORT 13               // RED LED is used for monitoring ethernet
 #define FLASH_TIME 300
 unsigned long NEXT_STATUS_TOGGLE_TIMER = 0;
 bool GREEN_LED_STATE = false;
 bool RED_LED_STATE = false;
 unsigned long timeSinceRedLedChanged = 0;
 
+bool SWITCH_STATE = false;
+long SWITCH_DEBOUNCE_FINISH = 0;
+long SWITCH_DEBOUNCE_LENGTH = 20;
 void setup() {
 
 
   // Using manual reset instead of tying to Arudino Reset
-  pinMode(53,OUTPUT);
-  digitalWrite(53,LOW);
+  pinMode(53, OUTPUT);
+  digitalWrite(53, LOW);
   delay(2);
-  digitalWrite(53,HIGH);
+  digitalWrite(53, HIGH);
+
+  pinMode(7, INPUT_PULLUP);
+  SWITCH_STATE = digitalRead(7);
 
   pinMode( RED_STATUS_LED_PORT,  OUTPUT);
   pinMode( GREEN_STATUS_LED_PORT,  OUTPUT);
@@ -32,27 +38,40 @@ void setup() {
 
   delay(FLASH_TIME);
 
-  Ethernet.begin(mac,ip);
+  Ethernet.begin(mac, ip);
 
+  Serial2.begin(9600);
 
 }
 
 void loop() {
 
-    if (millis() >= NEXT_STATUS_TOGGLE_TIMER) {
+  if (millis() >= NEXT_STATUS_TOGGLE_TIMER) {
     GREEN_LED_STATE = !GREEN_LED_STATE;
     RED_LED_STATE = !RED_LED_STATE;
     digitalWrite( GREEN_STATUS_LED_PORT, GREEN_LED_STATE);
-    digitalWrite( RED_STATUS_LED_PORT, RED_LED_STATE);
+    // digitalWrite( RED_STATUS_LED_PORT, RED_LED_STATE);
     NEXT_STATUS_TOGGLE_TIMER = millis() + FLASH_TIME;
   }
 
-//  // Turn off Red status led after flashtime
-//  if ((RED_LED_STATE == true) && (millis() >= (timeSinceRedLedChanged + FLASH_TIME ) )) {
-//    digitalWrite( RED_STATUS_LED_PORT, false);
-//    RED_LED_STATE = false;
-//    timeSinceRedLedChanged = millis();
-//
-//  }
-  
+  if ( millis() >= SWITCH_DEBOUNCE_FINISH) {
+    if (digitalRead(7) != SWITCH_STATE) {
+      SWITCH_DEBOUNCE_FINISH = millis() + SWITCH_DEBOUNCE_LENGTH;
+      SWITCH_STATE = digitalRead(7);
+      RED_LED_STATE = SWITCH_STATE;
+      digitalWrite( RED_STATUS_LED_PORT, RED_LED_STATE);
+      if (SWITCH_STATE == true) {
+        Serial2.println("* 0 IR 001\r");
+      }
+    }
+  }
+
+  //  // Turn off Red status led after flashtime
+  //  if ((RED_LED_STATE == true) && (millis() >= (timeSinceRedLedChanged + FLASH_TIME ) )) {
+  //    digitalWrite( RED_STATUS_LED_PORT, false);
+  //    RED_LED_STATE = false;
+  //    timeSinceRedLedChanged = millis();
+  //
+  //  }
+
 }
