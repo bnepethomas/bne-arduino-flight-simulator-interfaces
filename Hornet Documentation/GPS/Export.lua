fb2ccee6d57a6730cb9c98c734a37500cc589e53
bnepethomas <bnepethomas@gmail.com>
2023-06-25 09:18:16 +1000

    Update Export.lua

--- Hornet Documentation/GPS/Export.lua
+++ Hornet Documentation/GPS/Export.lua
@@ -30,17 +30,23 @@ function LuaExportActivityNextEvent(t)
 
 	
 	local gps_export_flightData = {}
-	table.insert(gps_export_flightData,"latitude:"..LoGetSelfData().LatLongAlt.Lat)       -- LATITUDE
-    table.insert(gps_export_flightData,"longitude:"..LoGetSelfData().LatLongAlt.Long)      -- LONGITUDE
-    table.insert(gps_export_flightData,"altitude:"..LoGetAltitudeAboveSeaLevel())          -- ALTITUDE SEA LEVEL(MTS TO FT)
-    table.insert(gps_export_flightData,"airspeed:"..LoGetTrueAirSpeed() * 1.94)            -- TRUE AIRSPEED (M/S TO KNOTS)
-    table.insert(gps_export_flightData,"magheading:"..LoGetSelfData().Heading * 57.3)         -- HEADING (RAD TO DEG)
-	gps_export_packet =  gps_export_flightData[1] .. "," .. gps_export_flightData[2] .. "," .. gps_export_flightData[3] .. ","
-	gps_export_packet =  gps_export_packet .. gps_export_flightData[4] .. "," .. gps_export_flightData[5] 
+	-- LATITUDE
+	table.insert(gps_export_flightData,"latitude:"..LoGetSelfData().LatLongAlt.Lat) 
+	-- LONGITUDE
+    table.insert(gps_export_flightData,"longitude:"..LoGetSelfData().LatLongAlt.Long)   
+	-- ALTITUDE SEA LEVEL(MTS TO FT)
+    table.insert(gps_export_flightData,"altitude:"..LoGetAltitudeAboveSeaLevel())
+	-- TRUE AIRSPEED (M/S TO KNOTS)
+    table.insert(gps_export_flightData,"airspeed:"..LoGetTrueAirSpeed() * 1.94)  
+	-- HEADING (RAD TO DEG)
+    table.insert(gps_export_flightData,"magheading:"..LoGetSelfData().Heading * 57.3)         
+	gps_export_packet =  gps_export_flightData[1] .. "," .. gps_export_flightData[2] .. "," 
+	gps_export_packet =  gps_export_packet .. gps_export_flightData[3] .. ","
+	gps_export_packet =  gps_export_packet .. gps_export_flightData[4] .. "," 
+	gps_export_packet =  gps_export_packet .. gps_export_flightData[5]
 	gps_export_socket.try(gps_export_con:send(gps_export_packet))
 	
 
-	
 	tNext = tNext + 1.0
 
 	return tNext
