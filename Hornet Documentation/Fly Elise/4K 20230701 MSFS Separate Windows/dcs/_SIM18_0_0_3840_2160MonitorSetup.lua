a29c334112fd5e12f7648183d44831a0c70026f5
bnepethomas <bnepethomas@gmail.com>
2023-07-09 09:45:47 +1000

    screen

---
+++ Hornet Documentation/Fly Elise/4K 20230701 MSFS Separate Windows/dcs/_SIM18_0_0_3840_2160MonitorSetup.lua
@@ -0,0 +1,21 @@
+_ = function(p) return p; end;
+name = _('SIM18_0_0_3840_2160');
+Description = 'Exported configuration with 1 projectors'
+Viewports =
+{
+	P1 =
+	{
+		x = 0;
+		y = 0;
+		width = 3840;
+		height = 2160;
+		aspect = 1.777778;
+		useAbsoluteFOV = true;
+		useAbsoluteAnglesShift = true;
+		FOV = 2.365660;
+		viewDx = 1.230527;
+		viewDy = 0.035436;
+	}
+}
+UIMainView = Viewports.P1
+
