cf308892ca42c3af3abbe47f9b65caef50b73cf2
bnepethomas <peterthomas@Peters-Mac-mini-2765.local>
2023-11-05 12:34:20 +1000

    A10

---
+++ A10/A10 DCS BIOS Sketches/A10_RIGHT_CONSOLE_OUTPUTS/A10_RIGHT_CONSOLE_OUTPUTS.ino
@@ -0,0 +1,44 @@
+#include <SPI.h>
+#include <Ethernet.h>
+
+byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
+IPAddress ip(172, 16, 1, 11);
+
+// THE LED PORTS WILL CHANGE FROM THE V1.1 PCB TO THE FOLLOWING
+// #define RED_STATUS_LED_PORT 12
+// #define GREEN_STATUS_LED_PORT 13
+#define RED_STATUS_LED_PORT 6
+#define GREEN_STATUS_LED_PORT 5
+#define FLASH_TIME 300
+
+unsigned long NEXT_STATUS_TOGGLE_TIMER = 0;
+bool GREEN_LED_STATE = false;
+bool RED_LED_STATE = false;
+unsigned long timeSinceRedLedChanged = 0;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(RED_STATUS_LED_PORT, OUTPUT);
+  pinMode(GREEN_STATUS_LED_PORT, OUTPUT);
+
+  digitalWrite(RED_STATUS_LED_PORT, true);
+  digitalWrite(GREEN_STATUS_LED_PORT, true);
+  delay(FLASH_TIME);
+  digitalWrite(RED_STATUS_LED_PORT, false);
+  digitalWrite(GREEN_STATUS_LED_PORT, false);
+
+  delay(FLASH_TIME);
+
+  Ethernet.begin(mac, ip);
+}
+
+void loop() {
+
+  if (millis() >= NEXT_STATUS_TOGGLE_TIMER) {
+    GREEN_LED_STATE = !GREEN_LED_STATE;
+    RED_LED_STATE = !RED_LED_STATE;
+    digitalWrite(GREEN_STATUS_LED_PORT, GREEN_LED_STATE);
+    // digitalWrite( RED_STATUS_LED_PORT, RED_LED_STATE);
+    NEXT_STATUS_TOGGLE_TIMER = millis() + FLASH_TIME;
+  }
+}
