8b9816d21e70bc9a8354f5211a81785d4729d999
bnepethomas <peterthomas@Peters-Mac-mini-2765.local>
2023-11-05 14:48:29 +1000

    Update A10_RIGHT_CONSOLE_OUTPUTS.ino

--- A10/A10 DCS BIOS Sketches/A10_RIGHT_CONSOLE_OUTPUTS/A10_RIGHT_CONSOLE_OUTPUTS.ino
+++ A10/A10 DCS BIOS Sketches/A10_RIGHT_CONSOLE_OUTPUTS/A10_RIGHT_CONSOLE_OUTPUTS.ino
@@ -1,8 +1,52 @@
+
+#define Ethernet_In_Use 1
+const int Serial_In_Use = 0;
+#define Reflector_In_Use 1
+
+
+// ###################################### Begin Ethernet Related #############################
 #include <SPI.h>
 #include <Ethernet.h>
+#include <EthernetUdp.h>
 
-byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
-IPAddress ip(172, 16, 1, 11);
+// These local Mac and IP Address will be reassigned early in startup based on
+// the device ID as set by address pins
+#define EthernetStartupDelay 500
+byte mac[] = { 0xA8, 0x61, 0x0A, 0x9E, 0x83, 0x06 };
+IPAddress ip(172, 16, 1, 106);
+String strMyIP = "172.16.1.106";
+
+// Reflector
+IPAddress reflectorIP(172, 16, 1, 10);
+String strReflectorIP = "X.X.X.X";
+
+const unsigned int max7219port = 7788;
+const unsigned int remoteport = 49000;
+const unsigned int reflectorport = 27000;
+
+const unsigned int MSFSport = 13136;
+
+// Packet Length
+int max7219packetsize;
+int max7219Len;
+
+int MSFSpacketsize;
+int MSFSLen;
+
+EthernetUDP max7219udp;  // Max7219
+EthernetUDP MSFSudp;     // Listens to MSFS light commands
+
+char max7219packetBuffer[1000];  //buffer to store packet data for both Max7219 and MSFS data
+char outpacketBuffer[1000];      //buffer to store the outgoing data
+
+bool Debug_Display = false;
+char *ParameterNamePtr;
+char *ParameterValuePtr;
+// ###################################### End Ethernet Related #############################
+
+
+
+
 
 // THE LED PORTS WILL CHANGE FROM THE V1.1 PCB TO THE FOLLOWING
 // #define RED_STATUS_LED_PORT 12
@@ -15,6 +59,158 @@ unsigned long NEXT_STATUS_TOGGLE_TIMER = 0;
 bool GREEN_LED_STATE = false;
 bool RED_LED_STATE = false;
 unsigned long timeSinceRedLedChanged = 0;
+
+#define DCSBIOS_IRQ_SERIAL
+
+// Pinouts for Version 4 PCB
+#define MAP_LIGHTS 6
+#define NVG_LIGHTS 6
+#define FLOOD_LIGHTS 7
+#define FORMATION_LIGHTS 8
+#define STROBE_LIGHTS 44
+#define NAVIGATION_LIGHTS 45
+#define BACK_LIGHTS 46
+
+
+#include "LedControl.h"
+#include "DcsBios.h"
+int devices = 2;
+
+
+#define DAY_MODE 0
+#define NIGHT_MODE 1
+#define NVG_MODE 2
+#define FULL_BRIGHTNESS 15
+
+#define STROBE_BRIGHT 2
+#define STROBE_DIM 0
+#define STROBE_BRIGHT_LEVEL 255
+#define STROBE_DIM_LEVEL 20
+
+int WARN_CAUTION_DIMMER_VALUE = 15;
+int AOA_DIMMER_VALUE = 15;
+int DAY_NIGHT_SWITCH_MODE = DAY_MODE;
+int NEW_AOA_DIMMER_VALUE = 15;
+int STROBE_BRIGHT_SWITCH_POS = STROBE_BRIGHT;
+long POSITION_BRIGHT_POT_POS = 65534;
+bool POSITION_LIGHTS_STATUS = true;
+
+LedControl lc = LedControl(16, 14, 15, devices);
+
+/* paste code snippets from the reference documentation here */
+//DcsBios::Switch2Pos lightsTestSw("LIGHTS_TEST_SW", 22);
+//DcsBios::LED sjCtrLt(0x742e, 0x4000, 13);
+// CHIP U1 (C)
+#define SELECT_JET_PANEL 2
+#define AOA 2  // NOT DONE
+#define MASTER_ARM 2
+
+// CHIP U2 (A)
+#define LEFT_EWI 0
+#define UFC_PANEL 0
+#define BIT_LED 0
+#define LEFT_DIS 0  // CHECK IF NEEDED
+#define Bit_led 0   // Bit Test Ledt HUD BOX
+
+#define LEFT_GEAR_COL_A 2
+#define LEFT_GEAR_ROW_A 1
+#define LEFT_GEAR_COL_B 1
+#define LEFT_GEAR_ROW_B 2
+
+
+void onFlpLgRightGearLtChange(unsigned int newValue) {
+  lc.setLed(SELECT_JET_PANEL, LEFT_GEAR_COL_A, LEFT_GEAR_ROW_A, newValue);
+  lc.setLed(SELECT_JET_PANEL, LEFT_GEAR_COL_B, LEFT_GEAR_ROW_B, newValue);
+}
+DcsBios::IntegerBuffer flpLgRightGearLtBuffer(0x7430, 0x2000, 13, onFlpLgRightGearLtChange);
+
+void onWarnCautionDimmerChange(unsigned int newValue) {
+
+  WARN_CAUTION_DIMMER_VALUE = map(newValue, 0, 65000, 0, 15);
+  updateBrightness();
+}
+DcsBios::IntegerBuffer warnCautionDimmerBuffer(0x754c, 0xffff, 0, onWarnCautionDimmerChange);
+
+void onCockkpitLightModeSwChangeMax7219(unsigned int newValue) {
+
+  // Called from onCockkpitLightModeSwChange in IFEI block
+
+  if (newValue == NVG_MODE) {
+    DAY_NIGHT_SWITCH_MODE = NVG_MODE;
+  } else if (newValue == NIGHT_MODE) {
+    DAY_NIGHT_SWITCH_MODE = NIGHT_MODE;
+  } else {
+    DAY_NIGHT_SWITCH_MODE = DAY_MODE;
+  }
+  updateBrightness();
+}
+
+
+void updateBrightness() {
+
+  if (Reflector_In_Use == 1) {
+    max7219udp.beginPacket(reflectorIP, reflectorport);
+    max7219udp.println("Entering update brightness");
+    max7219udp.endPacket();
+  }
+
+  if (Reflector_In_Use == 1) {
+    max7219udp.beginPacket(reflectorIP, reflectorport);
+    max7219udp.println("Procssing Dimmer Packet: " + String(AOA_DIMMER_VALUE));
+    max7219udp.endPacket();
+  }
+
+
+  if (Reflector_In_Use == 1) {
+    max7219udp.beginPacket(reflectorIP, reflectorport);
+    max7219udp.println("Warning: " + String(WARN_CAUTION_DIMMER_VALUE));
+    max7219udp.println("AOA    : " + String(AOA_DIMMER_VALUE));
+    max7219udp.endPacket();
+  }
+
+  for (int address = 0; address < devices; address++) {
+
+    if (DAY_NIGHT_SWITCH_MODE == DAY_MODE) {
+      lc.setIntensity(address, FULL_BRIGHTNESS);
+    } else
+      lc.setIntensity(address, WARN_CAUTION_DIMMER_VALUE);
+  }
+}
+
+
+
+void AllOn() {
+  for (int displayunit = 0; displayunit < 9; displayunit++) {
+    for (int row = 0; row < 8; row++) {
+      for (int col = 0; col < 8; col++) {
+        if (col != 9 && col != 9 && col != 9)
+          lc.setLed(displayunit, row, col, true);
+      }
+    }
+  }
+}
+
+
+void AllOff() {
+  for (int displayunit = 0; displayunit < 9; displayunit++) {
+    for (int row = 0; row < 8; row++) {
+      for (int col = 0; col < 8; col++) {
+        lc.setLed(displayunit, row, col, false);
+      }
+    }
+  }
+}
+
+
+void SetBrightness(int Brightness) {
+  for (int address = 0; address < devices; address++) {
+    lc.setIntensity(address, (Brightness));
+  }
+}
+
+// ************************************ End Max7219
+
+
 
 void setup() {
   // put your setup code here, to run once:
@@ -29,6 +225,40 @@ void setup() {
 
   delay(FLASH_TIME);
 
+  // Initialise the Max7219
+  devices = lc.getDeviceCount();
+
+  for (int address = 0; address < devices; address++) {
+    /*The MAX72XX is in power-saving mode on startup*/
+    lc.shutdown(address, false);
+    /* Set the brightness to a medium values */
+    lc.setIntensity(address, 8);
+    /* and clear the display */
+    lc.clearDisplay(address);
+  }
+  
+  AllOn();
+  delay(2000);
+
+
+  // Slowly Dim the Leds
+  for (int Local_Brightness = 15; Local_Brightness >= 0; Local_Brightness--) {
+    analogWrite(FORMATION_LIGHTS, map(Local_Brightness, 0, 15, 0, 255));
+    analogWrite(NAVIGATION_LIGHTS, map(Local_Brightness, 0, 15, 0, 255));
+    analogWrite(NVG_LIGHTS, map(Local_Brightness, 0, 15, 0, 255));
+    analogWrite(FLOOD_LIGHTS, map(Local_Brightness, 0, 15, 0, 255));
+    analogWrite(BACK_LIGHTS, map(Local_Brightness, 0, 15, 0, 255));
+    analogWrite(STROBE_LIGHTS, map(Local_Brightness, 0, 15, 0, 255));
+    SetBrightness(Local_Brightness);
+
+    delay(300);
+  }
+
+  // Turn off All Leds and set to mid brightness
+  AllOff();
+  SetBrightness(8);
+
+
   Ethernet.begin(mac, ip);
 }
 
@@ -38,7 +268,7 @@ void loop() {
     GREEN_LED_STATE = !GREEN_LED_STATE;
     RED_LED_STATE = !RED_LED_STATE;
     digitalWrite(GREEN_STATUS_LED_PORT, GREEN_LED_STATE);
-    // digitalWrite( RED_STATUS_LED_PORT, RED_LED_STATE);
+
     NEXT_STATUS_TOGGLE_TIMER = millis() + FLASH_TIME;
   }
 }
