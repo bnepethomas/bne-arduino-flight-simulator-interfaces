f885b2bb263f043645fcc201c6e5e31f73d4abf6
bnepethomas <peterthomas@Peters-Mac-mini-2765.local>
2024-02-03 10:34:48 +1000

    Update A10_FRONT_CONSOLE_STEPPERS.ino

--- A10/A10 DCS BIOS Sketches/A10_FRONT_CONSOLE_STEPPERS/A10_FRONT_CONSOLE_STEPPERS.ino
+++ A10/A10 DCS BIOS Sketches/A10_FRONT_CONSOLE_STEPPERS/A10_FRONT_CONSOLE_STEPPERS.ino
@@ -6,22 +6,34 @@
 // 0.19A with 4 steppers disabled
 // 0.27A with 4 steppers enabled
 // So average current draw 20mA per stepper
-// 4 Steppers direct with 4 driven by buffer
+// With 12 Steppers enabled - total peak into board was 490mA with average around 440mA
+// With Mega, Ethernet and Led running current was 220mA
+// With Mega and Ehternet current is 210mA
+// If steppers as disabled using the accelstepper library then current draw returns to 210mA
 
-// Found fuel flow wn't nicely run through L293D, so there are 7 steppers
-// After soak test for approx 5 minutes steppers lost sanity
-// USB Meter indicating 4.77 Volts 0.62A
-// will test by baselining time to loose sanity, and then reduce number of active steppers
-// Perhaps overheating or just on margins with timing
 
-// 20240128 Powered Mac Mac - guages failing within a couple of minutes
-// Testing with iPhone charger - after 4 to 5 minutes it was failing
+
+
+// Issues with steppers gettting out of synch was largely due to physical binding
+// which sometimes only appeared after a 30 minutes run.  Shaft was bings against faceplate.
 
 // 312 MicroSeconds (~0.3mS) to run through stepper loop with a single stepper is needing to step
 // 1344 MicroSeconds (~1.3mS) to run through stepper loop with 6 steppers needing to step
 // 2576 MicroSecond (~3mS) to run through loop when 12 steppers are needing to step
 
 
+// Stepper 1 - Left Engine PSI
+// Stepper 2 - Right Engine PSI
+// Stepper 3 - APU RPM
+// Stepper 4 - APU EGT
+// Stepper 5 - Left Engine RPM
+// Stepper 6 - Right Engine RPM
+// Stepper 7 - Left Engine Fuel Flow
+// Stepper 8 - Right Engine Fuel Flow
+// Stepper 9 - Left Engine Temp
+// Stepper 10 - Right Engine Temp
+// Stepper 11 - Left Engine Fan RPM
+// Stepper 12 - Right Engine Fan RPM
 
 
 #define Ethernet_In_Use 1
@@ -117,21 +129,6 @@ unsigned long TimeToComplete = 0;
 // Gives a total
 
 
-// Works but slow max speed of 30
-// #define COIL_RIGHT_HYD_A1 23
-// #define COIL_RIGHT_HYD_A2 25
-// #define COIL_RIGHT_HYD_A3 27
-// #define COIL_RIGHT_HYD_A4 29
-// //  stepperSTANDBY_ALT.setMaxSpeed(30);
-
-// Works but slow max speed of 30
-// #define COIL_RIGHT_HYD_A1 25
-// #define COIL_RIGHT_HYD_A2 23
-// #define COIL_RIGHT_HYD_A3 27
-// #define COIL_RIGHT_HYD_A4 29
-
-// Works but slow max speed of 30
-
 // If the stepper is incorrectly configured it will chatter above
 // rates of more than 30 steps per minute, if when correctly
 // configured speeds can exceed 2300
@@ -152,8 +149,21 @@ unsigned long TimeToComplete = 0;
 #define S11InUse true
 #define S12InUse true
 
+// Stepper 1 - Left Engine PSI
+// Stepper 2 - Right Engine PSI
+// Stepper 3 - APU RPM
+// Stepper 4 - APU EGT
+// Stepper 5 - Left Engine RPM
+// Stepper 6 - Right Engine RPM
+// Stepper 7 - Left Engine Fuel Flow
+// Stepper 8 - Right Engine Fuel Flow
+// Stepper 9 - Left Engine Temp
+// Stepper 10 - Right Engine Temp
+// Stepper 11 - Left Engine Fan RPM
+// Stepper 12 - Right Engine Fan RPM
 
-// #define S1InUse true
+
+// #define S1InUse false
 // #define S2InUse false
 // #define S3InUse false
 // #define S4InUse false
@@ -167,6 +177,9 @@ unsigned long TimeToComplete = 0;
 // #define S10InUse false
 // #define S11InUse false
 // #define S12InUse false
+
+
+
 
 #define COIL_STEPPER_1_A 12
 #define COIL_STEPPER_1_B 13
@@ -246,6 +259,15 @@ AccelStepper STEPPER_10(AccelStepper::FULL4WIRE, COIL_STEPPER_10_A, COIL_STEPPER
 AccelStepper STEPPER_11(AccelStepper::FULL4WIRE, COIL_STEPPER_11_A, COIL_STEPPER_11_B, COIL_STEPPER_11_C, COIL_STEPPER_11_D);
 AccelStepper STEPPER_12(AccelStepper::FULL4WIRE, COIL_STEPPER_12_A, COIL_STEPPER_12_B, COIL_STEPPER_12_C, COIL_STEPPER_12_D);
 
+
+void setZeroPoints() {
+  if (S8InUse) STEPPER_8.moveTo(10);
+
+  moveSteppersUntilTargetReached();
+  if (S8InUse) STEPPER_8.setCurrentPosition(0);
+}
+
+
 // ###################################### End Stepper Related #############################
 
 
@@ -310,28 +332,157 @@ void setup() {
   STEPPER_12.setAcceleration(STEPPER_ACCELERATION);
 
 
-  cycleSteppers(630);
+  initialiseAllSteppers(630);
+
+  setZeroPoints();
 }
 
-void cycleSteppers(int numberOfDegrees) {
+void moveSteppersUntilTargetReached() {
+  while ((STEPPER_1.distanceToGo() != 0) || (STEPPER_2.distanceToGo() != 0) || (STEPPER_3.distanceToGo() != 0) || (STEPPER_4.distanceToGo() != 0) || (STEPPER_5.distanceToGo() != 0) || (STEPPER_6.distanceToGo() != 0) || (STEPPER_7.distanceToGo() != 0) || (STEPPER_8.distanceToGo() != 0) || (STEPPER_9.distanceToGo() != 0) || (STEPPER_9.distanceToGo() != 0) || (STEPPER_10.distanceToGo() != 0) || (STEPPER_11.distanceToGo() != 0) || (STEPPER_12.distanceToGo() != 0)) {
+    //SendDebug("Stepper_1 distance to go :" + String(STEPPER_1.distanceToGo()));
+    if (TimingCollected == false) {
+      TimeToComplete = micros();
+    }
+
+
+    STEPPER_1.run();
+    STEPPER_2.run();
+    STEPPER_3.run();
+    STEPPER_4.run();
+    STEPPER_5.run();
+    STEPPER_6.run();
+    STEPPER_7.run();
+    STEPPER_8.run();
+    STEPPER_9.run();
+    STEPPER_10.run();
+    STEPPER_11.run();
+    STEPPER_12.run();
+    if (TimingCollected == false) {
+      TimeToComplete = micros() - TimeToComplete;
+      SendDebug("Time to run through cycle: " + String(TimeToComplete));
+      TimingCollected = true;
+    }
+  }
+}
+
+
+#define LeftEngPSIMaxDegrees 490
+void setLeftEngPSI(int TargetDegrees) {
+  if (S1InUse) {
+    if (TargetDegrees >= LeftEngPSIMaxDegrees) TargetDegrees = LeftEngPSIMaxDegrees;
+    STEPPER_1.moveTo(TargetDegrees);
+  }
+}
+
+#define RightEngPSIMaxDegrees 490
+void setRightEngPSI(int TargetDegrees) {
+  if (S2InUse) {
+    if (TargetDegrees >= RightEngPSIMaxDegrees) TargetDegrees = RightEngPSIMaxDegrees;
+    STEPPER_2.moveTo(TargetDegrees);
+  }
+}
+
+#define APURPMMaxDegrees 490
+void setAPURPM(int TargetDegrees) {
+  if (S3InUse) {
+    if (TargetDegrees >= APURPMMaxDegrees) TargetDegrees = APURPMMaxDegrees;
+    STEPPER_3.moveTo(TargetDegrees);
+  }
+}
+
+#define APUEGTMaxDegrees 500
+void setAPUEGT(int TargetDegrees) {
+  if (S4InUse) {
+    if (TargetDegrees >= APUEGTMaxDegrees) TargetDegrees = APUEGTMaxDegrees;
+    STEPPER_4.moveTo(TargetDegrees);
+  }
+}
+
+#define LeftEngRPMMaxDegrees 550
+void setLeftEngRPM(int TargetDegrees) {
+  if (S5InUse) {
+    if (TargetDegrees >= LeftEngRPMMaxDegrees) TargetDegrees = LeftEngRPMMaxDegrees;
+    STEPPER_5.moveTo(TargetDegrees);
+  }
+}
+
+#define RightEngRPMMaxDegrees 550
+void setRightEngRPM(int TargetDegrees) {
+  if (S6InUse) {
+    if (TargetDegrees >= RightEngRPMMaxDegrees) TargetDegrees = RightEngRPMMaxDegrees;
+    STEPPER_6.moveTo(TargetDegrees);
+  }
+}
+
+
+#define LeftFuelFlowMaxDegrees 630
+void setLeftFuelFlow(int TargetDegrees) {
+  if (S7InUse) {
+    if (TargetDegrees >= LeftFuelFlowMaxDegrees) TargetDegrees = LeftFuelFlowMaxDegrees;
+    STEPPER_7.moveTo(TargetDegrees);
+  }
+}
+
+#define rightFuelFlowMaxDegrees 630
+void setRightFuelFlow(int TargetDegrees) {
+  if (S8InUse) {
+    if (TargetDegrees >= rightFuelFlowMaxDegrees) TargetDegrees = rightFuelFlowMaxDegrees;
+    STEPPER_8.moveTo(TargetDegrees);
+  }
+}
+
+#define LeftEngTempMaxDegrees 480
+void setLeftEngTemp(int TargetDegrees) {
+  if (S9InUse) {
+    if (TargetDegrees >= LeftEngTempMaxDegrees) TargetDegrees = LeftEngTempMaxDegrees;
+    STEPPER_9.moveTo(TargetDegrees);
+  }
+}
+
+#define RightEngTempMaxDegrees 480
+void setRightEngTemp(int TargetDegrees) {
+  if (S10InUse) {
+    if (TargetDegrees >= RightEngTempMaxDegrees) TargetDegrees = RightEngTempMaxDegrees;
+    STEPPER_10.moveTo(TargetDegrees);
+  }
+}
+
+#define LeftFanRPMMaxDegrees 550
+void setLeftFanRPM(int TargetDegrees) {
+  if (S11InUse) {
+    if (TargetDegrees >= LeftFanRPMMaxDegrees) TargetDegrees = LeftFanRPMMaxDegrees;
+    STEPPER_11.moveTo(TargetDegrees);
+  }
+}
+
+#define RightFanRPMMaxDegrees 550
+void setRightFanRPM(int TargetDegrees) {
+  if (S12InUse) {
+    if (TargetDegrees >= RightFanRPMMaxDegrees) TargetDegrees = RightFanRPMMaxDegrees;
+    STEPPER_12.moveTo(TargetDegrees);
+  }
+}
+
+void initialiseAllSteppers(int numberOfDegrees) {
   // 630 for full sweep
+  // Do a full swing for all steppers to set 0 point
 
 
   //enableAllSteppers();
 
   SendDebug("Start All Stepper");
-  if (S1InUse) STEPPER_1.move(numberOfDegrees);
-  if (S2InUse) STEPPER_2.move(numberOfDegrees);
-  if (S3InUse) STEPPER_3.move(numberOfDegrees);
-  if (S4InUse) STEPPER_4.move(numberOfDegrees);
-  if (S5InUse) STEPPER_5.move(numberOfDegrees);
-  if (S6InUse) STEPPER_6.move(numberOfDegrees);
-  if (S7InUse) STEPPER_7.move(numberOfDegrees);
-  if (S8InUse) STEPPER_8.move(numberOfDegrees);
-  if (S9InUse) STEPPER_9.move(numberOfDegrees);
-  if (S10InUse) STEPPER_10.move(numberOfDegrees);
-  if (S11InUse) STEPPER_11.move(numberOfDegrees);
-  if (S12InUse) STEPPER_12.move(numberOfDegrees);
+  if (S1InUse) STEPPER_1.moveTo(numberOfDegrees);
+  if (S2InUse) STEPPER_2.moveTo(numberOfDegrees);
+  if (S3InUse) STEPPER_3.moveTo(numberOfDegrees);
+  if (S4InUse) STEPPER_4.moveTo(numberOfDegrees);
+  if (S5InUse) STEPPER_5.moveTo(numberOfDegrees);
+  if (S6InUse) STEPPER_6.moveTo(numberOfDegrees);
+  if (S7InUse) STEPPER_7.moveTo(numberOfDegrees);
+  if (S8InUse) STEPPER_8.moveTo(numberOfDegrees);
+  if (S9InUse) STEPPER_9.moveTo(numberOfDegrees);
+  if (S10InUse) STEPPER_10.moveTo(numberOfDegrees);
+  if (S11InUse) STEPPER_11.moveTo(numberOfDegrees);
+  if (S12InUse) STEPPER_12.moveTo(numberOfDegrees);
 
   while ((STEPPER_1.distanceToGo() != 0) || (STEPPER_2.distanceToGo() != 0) || (STEPPER_3.distanceToGo() != 0) || (STEPPER_4.distanceToGo() != 0) || (STEPPER_5.distanceToGo() != 0) || (STEPPER_6.distanceToGo() != 0) || (STEPPER_7.distanceToGo() != 0) || (STEPPER_8.distanceToGo() != 0) || (STEPPER_9.distanceToGo() != 0) || (STEPPER_9.distanceToGo() != 0) || (STEPPER_10.distanceToGo() != 0) || (STEPPER_11.distanceToGo() != 0) || (STEPPER_12.distanceToGo() != 0)) {
     //SendDebug("Stepper_1 distance to go :" + String(STEPPER_1.distanceToGo()));
@@ -358,20 +509,21 @@ void cycleSteppers(int numberOfDegrees) {
       TimingCollected = true;
     }
   }
-  SendDebug("Steppers at Max");
-  if (S1InUse) STEPPER_1.move(-numberOfDegrees);
-  if (S2InUse) STEPPER_2.move(-numberOfDegrees);
-  if (S3InUse) STEPPER_3.move(-numberOfDegrees);
-  if (S4InUse) STEPPER_4.move(-numberOfDegrees);
-  if (S5InUse) STEPPER_5.move(-numberOfDegrees);
-  if (S6InUse) STEPPER_6.move(-numberOfDegrees);
-  if (S7InUse) STEPPER_7.move(-numberOfDegrees);
-  if (S8InUse) STEPPER_8.move(-numberOfDegrees);
-  if (S9InUse) STEPPER_9.move(-numberOfDegrees);
-  if (S10InUse) STEPPER_10.move(-numberOfDegrees);
-  if (S11InUse) STEPPER_11.move(-numberOfDegrees);
-  if (S12InUse) STEPPER_12.move(-numberOfDegrees);
+  // SendDebug("Steppers at Max");
+  if (S1InUse) STEPPER_1.moveTo(0);
+  if (S2InUse) STEPPER_2.moveTo(0);
+  if (S3InUse) STEPPER_3.moveTo(0);
+  if (S4InUse) STEPPER_4.moveTo(0);
+  if (S5InUse) STEPPER_5.moveTo(0);
+  if (S6InUse) STEPPER_6.moveTo(0);
+  if (S7InUse) STEPPER_7.moveTo(0);
+  if (S8InUse) STEPPER_8.moveTo(0);
+  if (S9InUse) STEPPER_9.moveTo(0);
+  if (S10InUse) STEPPER_10.moveTo(0);
+  if (S11InUse) STEPPER_11.moveTo(0);
+  if (S12InUse) STEPPER_12.moveTo(0);
   //SendDebug("Returning");
+
   while ((STEPPER_1.distanceToGo() != 0) || (STEPPER_2.distanceToGo() != 0) || (STEPPER_3.distanceToGo() != 0) || (STEPPER_4.distanceToGo() != 0) || (STEPPER_5.distanceToGo() != 0) || (STEPPER_6.distanceToGo() != 0) || (STEPPER_7.distanceToGo() != 0) || (STEPPER_8.distanceToGo() != 0) || (STEPPER_9.distanceToGo() != 0) || (STEPPER_9.distanceToGo() != 0) || (STEPPER_10.distanceToGo() != 0) || (STEPPER_11.distanceToGo() != 0) || (STEPPER_12.distanceToGo() != 0))
 
   {
@@ -392,7 +544,88 @@ void cycleSteppers(int numberOfDegrees) {
   SendDebug("End All Stepper");
 }
 
-void enableAllSteppers() {
+
+void cycleSteppers(int numberOfDegrees) {
+  // 630 for full sweep
+
+
+  //enableAllSteppers();
+
+  SendDebug("Start All Stepper");
+  if (S1InUse) setLeftEngPSI(numberOfDegrees);
+  if (S2InUse) setRightEngPSI(numberOfDegrees);
+  if (S3InUse) setAPURPM(numberOfDegrees);
+  if (S4InUse) setAPUEGT(numberOfDegrees);
+  if (S5InUse) setLeftEngRPM(numberOfDegrees);
+  if (S6InUse) setRightEngRPM(numberOfDegrees);
+  if (S7InUse) setLeftFuelFlow(numberOfDegrees);
+  if (S8InUse) setRightFuelFlow(numberOfDegrees);
+  if (S9InUse) setLeftEngTemp(numberOfDegrees);
+  if (S10InUse) setRightEngTemp(numberOfDegrees);
+  if (S11InUse) setLeftFanRPM(numberOfDegrees);
+  if (S12InUse) setRightFanRPM(numberOfDegrees);
+
+  while ((STEPPER_1.distanceToGo() != 0) || (STEPPER_2.distanceToGo() != 0) || (STEPPER_3.distanceToGo() != 0) || (STEPPER_4.distanceToGo() != 0) || (STEPPER_5.distanceToGo() != 0) || (STEPPER_6.distanceToGo() != 0) || (STEPPER_7.distanceToGo() != 0) || (STEPPER_8.distanceToGo() != 0) || (STEPPER_9.distanceToGo() != 0) || (STEPPER_9.distanceToGo() != 0) || (STEPPER_10.distanceToGo() != 0) || (STEPPER_11.distanceToGo() != 0) || (STEPPER_12.distanceToGo() != 0)) {
+    //SendDebug("Stepper_1 distance to go :" + String(STEPPER_1.distanceToGo()));
+    if (TimingCollected == false) {
+      TimeToComplete = micros();
+    }
+
+
+    STEPPER_1.run();
+    STEPPER_2.run();
+    STEPPER_3.run();
+    STEPPER_4.run();
+    STEPPER_5.run();
+    STEPPER_6.run();
+    STEPPER_7.run();
+    STEPPER_8.run();
+    STEPPER_9.run();
+    STEPPER_10.run();
+    STEPPER_11.run();
+    STEPPER_12.run();
+    if (TimingCollected == false) {
+      TimeToComplete = micros() - TimeToComplete;
+      SendDebug("Time to run through cycle: " + String(TimeToComplete));
+      TimingCollected = true;
+    }
+  }
+  // SendDebug("Steppers at Max");
+  if (S1InUse) STEPPER_1.moveTo(0);
+  if (S2InUse) STEPPER_2.moveTo(0);
+  if (S3InUse) STEPPER_3.moveTo(0);
+  if (S4InUse) STEPPER_4.moveTo(0);
+  if (S5InUse) STEPPER_5.moveTo(0);
+  if (S6InUse) STEPPER_6.moveTo(0);
+  if (S7InUse) STEPPER_7.moveTo(0);
+  if (S8InUse) STEPPER_8.moveTo(0);
+  if (S9InUse) STEPPER_9.moveTo(0);
+  if (S10InUse) STEPPER_10.moveTo(0);
+  if (S11InUse) STEPPER_11.moveTo(0);
+  if (S12InUse) STEPPER_12.moveTo(0);
+  //SendDebug("Returning");
+
+  while ((STEPPER_1.distanceToGo() != 0) || (STEPPER_2.distanceToGo() != 0) || (STEPPER_3.distanceToGo() != 0) || (STEPPER_4.distanceToGo() != 0) || (STEPPER_5.distanceToGo() != 0) || (STEPPER_6.distanceToGo() != 0) || (STEPPER_7.distanceToGo() != 0) || (STEPPER_8.distanceToGo() != 0) || (STEPPER_9.distanceToGo() != 0) || (STEPPER_9.distanceToGo() != 0) || (STEPPER_10.distanceToGo() != 0) || (STEPPER_11.distanceToGo() != 0) || (STEPPER_12.distanceToGo() != 0))
+
+  {
+    STEPPER_1.run();
+    STEPPER_2.run();
+    STEPPER_3.run();
+    STEPPER_4.run();
+    STEPPER_5.run();
+    STEPPER_6.run();
+    STEPPER_7.run();
+    STEPPER_8.run();
+    STEPPER_9.run();
+    STEPPER_10.run();
+    STEPPER_11.run();
+    STEPPER_12.run();
+  }
+  //disableAllSteppers();
+  SendDebug("End All Stepper");
+}
+
+void disableAllSteppers() {
   STEPPER_1.disableOutputs();
   STEPPER_2.disableOutputs();
   STEPPER_3.disableOutputs();
@@ -407,7 +640,7 @@ void enableAllSteppers() {
   STEPPER_12.disableOutputs();
 }
 
-void disableAllSteppers() {
+void enableAllSteppers() {
   STEPPER_1.enableOutputs();
   STEPPER_2.enableOutputs();
   STEPPER_3.enableOutputs();
@@ -427,7 +660,9 @@ void loop() {
   if (millis() >= NEXT_STATUS_TOGGLE_TIMER) {
     RED_LED_STATE = !RED_LED_STATE;
     digitalWrite(RED_STATUS_LED_PORT, RED_LED_STATE);
-    cycleSteppers(550);
+    enableAllSteppers();
+    cycleSteppers(650);
+    disableAllSteppers();
     SendDebug("Uptime " + String(millis()) + " (" + String(millis() / 60000) + ")");
     NEXT_STATUS_TOGGLE_TIMER = millis() + FLASH_TIME;
   }
