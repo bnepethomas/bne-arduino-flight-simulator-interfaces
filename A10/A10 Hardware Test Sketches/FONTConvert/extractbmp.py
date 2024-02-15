b8a519a65d6cacfb8f20ee7ff5d6e532f28c4a1f
bnepethomas <bnepethomas@gmail.com>
2024-02-15 19:46:59 +1000

    font

--- A10/A10 Hardware Test Sketches/FONTConvert/extractbmp.py
+++ A10/A10 Hardware Test Sketches/FONTConvert/extractbmp.py
@@ -11,7 +11,8 @@ def extract_bmp_part(input_file, output_file, x, y, width, height):
     cropped_img.save(output_file)
 
 if __name__ == "__main__":
-    input_file = "alldigits.bmp"
+    # input_file = "alldigits.bmp"
+    input_file = "HashOne.bmp"
     output_file = "output.bmp"
 
     # Define the coordinates and dimensions for the part to be extracted
@@ -21,6 +22,9 @@ if __name__ == "__main__":
     height = 48
 
     # Extract a part of the BMP file
-    extract_bmp_part(input_file, output_file, x, y, width, height)
+    for i in range(0,12,2):
+        # output_file = "output" + str(i) + ".bmp"
+        output_file = "HashOne" + str(i) + ".bmp"
+        extract_bmp_part(input_file, output_file, x, int(i * height/10) , width, height)
 
     print("Part extracted and saved to output.bmp")
