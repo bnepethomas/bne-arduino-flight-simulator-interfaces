857015f50211044d7f797c4ad97bc72817dfcd76
bnepethomas <bnepethomas@gmail.com>
2024-02-16 06:05:39 +1000

    font

--- A10/A10 Hardware Test Sketches/FONTConvert/extracttoc.py
+++ A10/A10 Hardware Test Sketches/FONTConvert/extracttoc.py
@@ -15,9 +15,21 @@ def export_to_c_format(image_data, width, height, output_file):
         f.write('#include <stdint.h>\n\n')
         f.write('const uint8_t image_data[] = {\n')
 
+        # Thiscode is mapping a pixel to a byte - needs to map 8 pixels to a byte
         for y in range(height):
-            for x in range(width):
-                pixel = image_data[y * width + x]
+            for x in range(0,width,8):
+                print(x)
+                pixel_0 = image_data[y * width + x]
+                pixel_1 = image_data[y * width + x + 1]
+                pixel_2 = image_data[y * width + x + 2]
+                pixel_3 = image_data[y * width + x + 3]
+                pixel_4 = image_data[y * width + x + 4]
+                pixel_5 = image_data[y * width + x + 5]
+                pixel_6 = image_data[y * width + x + 6]
+                pixel_7 = image_data[y * width + x + 7]
+
+                pixel = pixel_7 * 128 + pixel_6 * 64 + pixel_5 * 32 + pixel_4 * 16 + pixel_3 * 8 + pixel_2 * 4 + pixel_1 * 2 + pixel_0
+                
                 f.write(f'0x{pixel:02X}, ')
             f.write('\n')
 
@@ -26,7 +38,7 @@ def export_to_c_format(image_data, width, height, output_file):
         f.write(f'const int image_height = {height};\n')
 
 if __name__ == "__main__":
-    input_file = "48-48-1.bmp"
+    input_file = "48-48-2.bmp"
     output_file = "output.bmp"
     cropped_output_file = "cropped_output.bmp"
     c_output_file = "image_data.c"
