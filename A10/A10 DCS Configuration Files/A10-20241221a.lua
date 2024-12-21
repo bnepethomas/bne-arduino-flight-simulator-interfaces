8a0d495ecda29ad8666928bae3719370f329e852
bnepethomas <bnepethomas@gmail.com>
2024-12-22 08:09:43 +1000

    a10

---
+++ A10/A10 DCS Configuration Files/A10-20241221a.lua
@@ -0,0 +1,32 @@
+_  = function(p) return p end
+name = _('A10-20241221A')
+description = 'Generated from compatible Helios Profiles'
+Viewports = {
+  P2 = {
+    x = 0,
+    y = 0,
+    width = 1920,
+    height = 1080,
+    aspect = 1.77777777777778,
+    dx = 0,
+    dy = 0
+  }
+}
+LEFT_MFCD =
+{
+     x = 367;
+     y = 1151;
+     width = 800;
+     height = 600;
+}
+RIGHT_MFCD =
+{
+     x = 67;
+     y = 1872;
+     width = 800;
+     height = 600;
+}
+--UIMainView = Viewports.P1
+UI = { x = 0, y = 0, width = 1920, height = 1080 }
+UIMainView = UI
+--GUI_MAIN_VIEWPORT = Viewports.P2
---
+++ A10/A10 DCS Configuration Files/A10-20241222a.lua
@@ -0,0 +1,63 @@
+_  = function(p) return p end
+name = _('A10-20241222A')
+description = 'Generated from compatible Helios Profiles'
+Viewports = {
+  P2 = {
+    x = 0,
+    y = 0,
+    width = 1920,
+    height = 1080,
+    aspect = 1.77777777777778,
+    dx = 0,
+    dy = 0
+  }
+}
+LEFT_MFCD =
+{
+     x = 367;
+     y = 1151;
+     width = 800;
+     height = 600;
+}
+RIGHT_MFCD =
+{
+     x = 67;
+     y = 1872;
+     width = 800;
+     height = 600;
+}
+
+ED_A10C_CMSP =
+{
+		x = 1;
+		y = 1026;
+		width = 1;
+		height = 1;
+}
+
+ED_A10C_CMSC =
+{
+		x = 1;
+		y = 1026;
+		width = 1;
+		height = 1;
+}
+ED_A10C_RWR =
+{
+		x = 1257;
+		y = 1560;
+		width = 600;
+		height = 600;
+}
+ED_A10C_CDU =
+{
+		x = 1264;
+		y = 1088;
+		width = 640;
+		height = 480;
+}
+	
+--UIMainView = Viewports.P1
+UI = { x = 0, y = 0, width = 1920, height = 1080 }
+UIMainView = UI
+--GUI_MAIN_VIEWPORT = Viewports.P2
