1b86c212867b4c63b27ccd93b0efe356e00d41cb
bnepethomas <bnepethomas@gmail.com>
2024-12-22 09:19:30 +1000

    Update A10-20241222a.lua

--- A10/A10 DCS Configuration Files/A10-20241222a.lua
+++ A10/A10 DCS Configuration Files/A10-20241222a.lua
@@ -1,5 +1,5 @@
 _  = function(p) return p end
-name = _('A10-20241222A')
+name = _('A10-22')
 description = 'Generated from compatible Helios Profiles'
 Viewports = {
   P2 = {
@@ -26,37 +26,38 @@ RIGHT_MFCD =
      width = 800;
      height = 600;
 }
-
 ED_A10C_CMSP =
 {
-		x = 1;
-		y = 1026;
-		width = 1;
-		height = 1;
+	x = 1;
+	y = 1;
+	width = 50;
+	height = 50;
 }
-
 ED_A10C_CMSC =
 {
-		x = 1;
-		y = 1026;
-		width = 1;
-		height = 1;
-}
-ED_A10C_RWR =
-{
-		x = 1257;
-		y = 1560;
-		width = 600;
-		height = 600;
+	x = 1;
+	y = 51;
+	width = 50;
+	height = 50;
 }
 ED_A10C_CDU =
 {
-		x = 1264;
-		y = 1088;
-		width = 640;
-		height = 480;
+	--x = 1264;
+	--y = 1088;
+	--x = 1280;
+	--y = 1200;
+	x = 1310;
+	y = 1115;
+	width = 630;
+	height = 450;
+}	
+ED_A10C_RWR =
+{
+	x = 1257;
+	y = 1560;
+	width = 500;
+	height = 4800;
 }
-	
 --UIMainView = Viewports.P1
 UI = { x = 0, y = 0, width = 1920, height = 1080 }
 UIMainView = UI
