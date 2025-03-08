758878ec17796063996d1fcaf3fe77972d882151
bnepethomas <bnepethomas@gmail.com>
2025-03-08 11:47:41 +1100

    Update reset_Hornet_display_configurations.cmd

--- Hornet/Hornet DCS Configuration Files/reset_Hornet_display_configurations.cmd
+++ Hornet/Hornet DCS Configuration Files/reset_Hornet_display_configurations.cmd
@@ -1,9 +1,7 @@
 echo Copy Configuration files
 echo Had to change file attributes from read only and change permissions to mods sub directories
 echo CDU
-copy .\CDU_init.lua "C:\Program Files\Eagle Dynamics\DCS World\Mods\aircraft\A-10C\Cockpit\Scripts\CDU\indicator"
+copy .\IFEI_init.lua "D:\Program Files\Eagle Dynamics\DCS World\Mods\aircraft\FA-18C\Cockpit\Scripts\IFEI\indicator\IFEI_TIME_SET_page.lua"
 
-copy .\AN_ALR69V_init.lua  "C:\Program Files\Eagle Dynamics\DCS World\Mods\aircraft\A-10C\Cockpit\Scripts\AN_ALR69V\indicator"
-copy .\CMSC_init.lua "C:\Program Files\Eagle Dynamics\DCS World\Mods\aircraft\A-10C\Cockpit\Scripts\CMSC\indicator"
-copy .\CMSP_init.lua "C:\Program Files\Eagle Dynamics\DCS World\Mods\aircraft\A-10C\Cockpit\Scripts\CMSP\indicator"
+copy .\UFC_init.lua "D:\Program Files\Eagle Dynamics\DCS World\Mods\aircraft\FA-18C\Cockpit\Scripts\UFC\indicator"
 pause
