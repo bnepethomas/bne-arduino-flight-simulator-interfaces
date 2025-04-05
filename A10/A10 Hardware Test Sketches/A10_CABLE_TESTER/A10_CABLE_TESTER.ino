90fa540b97c49e013b78b87aa5c5b0508a24212a
bnepethomas <bnepethomas@gmail.com>
2025-04-05 16:09:00 +1000

    Update A10_CABLE_TESTER.ino

--- A10/A10 Hardware Test Sketches/A10_CABLE_TESTER/A10_CABLE_TESTER.ino
+++ A10/A10 Hardware Test Sketches/A10_CABLE_TESTER/A10_CABLE_TESTER.ino
@@ -82,6 +82,91 @@ char buffer[20];  //plenty of space for the value of millis() plus a zero termin
 
 char* ptrtopass;
 
+int txPin[30];
+int rxPin[30];
+
+void SetupPins() {
+  txPin[1] = 1;
+  txPin[2] = 2;
+  txPin[3] = 3;
+  txPin[4] = 4;
+  txPin[5] = 5;
+  txPin[6] = 6;
+  txPin[7] = 7;
+  txPin[8] = 8;
+  txPin[9] = 9;
+  txPin[10] = 10;
+  txPin[11] = 11;
+  txPin[12] = 12;
+  txPin[13] = 13;
+  txPin[14] = 14;
+  txPin[15] = 15;
+  txPin[16] = 69;  //A15
+  txPin[17] = 68;  //14
+  txPin[18] = 18;
+  txPin[19] = 19;
+  txPin[20] = 20;
+  txPin[21] = 21;
+  txPin[22] = 22;
+  txPin[23] = 23;
+  txPin[24] = 24;
+  txPin[25] = 25;
+  txPin[26] = 26;
+
+  rxPin[1] = 27;
+  rxPin[2] = 28;
+  rxPin[3] = 29;
+  rxPin[4] = 30;
+  rxPin[5] = 31;
+  rxPin[6] = 32;
+  rxPin[7] = 33;
+  rxPin[8] = 34;
+  rxPin[9] = 35;
+  rxPin[10] = 36;
+  rxPin[11] = 37;
+  rxPin[12] = 38;
+  rxPin[13] = 39;
+  rxPin[14] = 40;
+  rxPin[15] = 41;
+  rxPin[16] = 42;
+  rxPin[17] = 43;
+  rxPin[18] = 44;
+  rxPin[19] = 45;
+  rxPin[20] = 46;
+  rxPin[21] = 47;
+  rxPin[22] = 48;
+  rxPin[23] = 49;
+  rxPin[24] = 50;
+  rxPin[25] = 51;
+  rxPin[26] = 52;
+
+  for (int i = 1; i <= 26; i++) {
+    pinMode(rxPin[i], INPUT_PULLUP);
+  }
+
+  for (int i = 1; i <= 26; i++) {
+    pinMode(txPin[i], OUTPUT);
+  }
+}
+
+void txPinsToFloat() {
+  for (int i = 1; i <= 26; i++) {
+    pinMode(txPin[i], INPUT_PULLUP);
+  }
+}
+
+void AllPinsLow() {
+  for (int i = 1; i <= 26; i++) {
+    digitalWrite(txPin[i], LOW);
+  }
+}
+
+void AllPinsHigh() {
+  for (int i = 1; i <= 26; i++) {
+    digitalWrite(txPin[i], HIGH);
+  }
+}
+
 void tcaselect(uint8_t i) {
   if (i > 7) return;
 
@@ -91,13 +176,14 @@ void tcaselect(uint8_t i) {
 }
 
 void SendDebug(String MessageToSend) {
-  Serial.println(MessageToSend);
+  // Serial.println(MessageToSend);
 }
 
+
 void setup() {
-  Serial.begin(115200);
-  Serial.write("Cable Tester Start");
-  SendDebug("hello");
+  // Serial.begin(115200);
+  // Serial.write("Cable Tester Start");
+  // SendDebug("hello");
   pinMode(54, INPUT_PULLUP);
 
 
@@ -130,12 +216,9 @@ void setup() {
   u8g2_BARO.setFont(u8g2_font_logisoso16_tf);
   u8g2_BARO.sendBuffer();
   updateBARO("CABLE TEST INIT");
-  delay(1000);
-  updateBARO("STARTING CABLE TEST");
-  delay(1000);
-  updateBARO("CABLE IS 8 WIDE");
 
 
+  SetupPins();
 }
 
 
@@ -156,10 +239,85 @@ void updateBARO(String strnewValue) {
 
 
 void loop() {
- if(digitalRead(54) == LOW) {
-  updateBARO("Test Start");
-  delay(2000);
-  updateBARO("Test Complete");
- }
+  if (digitalRead(54) == LOW) {
+    bool errorEncountered = false;
+    updateBARO("Test Start");
+    delay(300);
 
+
+
+    // This test should pass without any cables connected
+    // Just checking that pins go high
+    AllPinsHigh();
+    for (int i = 1; i <= 26; i++) {
+      if (digitalRead(rxPin[i]) != HIGH) {
+        updateBARO("Pin " + String(i) + " not HIGH");
+        delay(5000);
+        errorEncountered = true;
+      };
+    }
+
+    updateBARO("All High");
+    delay(500);
+    AllPinsLow();
+    updateBARO("All Low");
+    delay(500);
+
+    // Find last connected pin
+    SendDebug("Finding last connected pin");
+    int LastConnectedPin = 0;
+    txPinsToFloat();
+    for (int i = 1; i <= 26; i++) {
+      updateBARO("Pin " + String(i));
+      //SendDebug("Setting Low Pin " + String(txPin[i]) + " Reading Pin " + String(rxPin[i]));
+      pinMode(txPin[i], OUTPUT);
+      digitalWrite(txPin[i], LOW);
+      delay(50);
+      if (digitalRead(rxPin[i]) == LOW) {
+        SendDebug("Pin " + String(rxPin[i]) + " LOW");
+        LastConnectedPin = i;
+      };
+      pinMode(txPin[i], INPUT);
+    }
+    if (LastConnectedPin == 0) {
+      updateBARO("No cable connected");
+      errorEncountered = true;
+    } else
+      updateBARO("Testing to " + String(LastConnectedPin));
+    delay(500);
+
+    // Test that only a single pin changes
+    if (errorEncountered != true) {
+      for (int i = 1; i <= LastConnectedPin; i++) {
+        updateBARO("Short Test " + String(txPin[i]));
+        delay(500);
+        txPinsToFloat();
+        pinMode(txPin[i], OUTPUT);
+        digitalWrite(txPin[i], LOW);
+        for (int j = 1; i <= LastConnectedPin; i++) {
+          
+          if (errorEncountered == true)
+            break;
+          if (j != i) {
+            // Checking to see if other pins have been pulled low
+            if (digitalRead(rxPin[j]) == LOW) {
+              // We have a different pin to what we expect going low
+              errorEncountered = true;
+
+              updateBARO("Short " + String(i) + ":" + String(j));
+              txPinsToFloat();
+              delay(10000);
+              break;
+            }
+          }
+        }
+        pinMode(txPin[i], INPUT);
+      }
+    }
+
+    if (errorEncountered == true)
+      updateBARO("Test Failed");
+    else
+      updateBARO("Pass to : " + String(LastConnectedPin));
+  }
 }
