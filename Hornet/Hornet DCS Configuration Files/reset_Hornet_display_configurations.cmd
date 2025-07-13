e003fbe0cf5cd03287e6d672ea6acced761b199a
bnepethomas <bnepethomas@gmail.com>
2025-07-14 05:40:06 +1000

    LUA

--- Hornet/Hornet DCS Configuration Files/reset_Hornet_display_configurations.cmd
+++ Hornet/Hornet DCS Configuration Files/reset_Hornet_display_configurations.cmd
@@ -4,4 +4,5 @@ echo CDU
 copy .\IFEI_init.lua "D:\Program Files\Eagle Dynamics\DCS World\Mods\aircraft\FA-18C\Cockpit\Scripts\IFEI\indicator\IFEI_TIME_SET_page.lua"
 
 copy .\UFC_init.lua "D:\Program Files\Eagle Dynamics\DCS World\Mods\aircraft\FA-18C\Cockpit\Scripts\UFC\indicator"
+copy .\RWR_ALR67_init.lua "D:\Program Files\Eagle Dynamics\DCS World\Mods\aircraft\FA-18C\Cockpit\Scripts\TEWS\indicator"
 pause
