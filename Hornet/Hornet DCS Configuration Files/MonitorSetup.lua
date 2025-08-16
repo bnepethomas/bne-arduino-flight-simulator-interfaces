f31c4dbf6cf309ab166217aba568a5598e11b251
bnepethomas <bnepethomas@gmail.com>
2025-08-16 11:21:36 +1000

    monitro config

--- Hornet/Hornet DCS Configuration Files/MonitorSetup.lua
+++ Hornet/Hornet DCS Configuration Files/MonitorSetup.lua
@@ -4,12 +4,13 @@ Description = 'Exported configuration with 3 projectors'
 
 -- First value is top left corner of the screen
 
-CENTER_MFCD = { x = 11520 + 0 , y = 0 + 445, width = 595, height = 590 }
-LEFT_MFCD = { x = 12120 + 17, y = 0 + 30, width = 560, height = 590 }
-RIGHT_MFCD = { x = 11520 + 460, y = 1050 + 30, width = 560, height = 580 }
-F18_RWR = { x = 12097, y = 1814, width = 430, height = 450 }
+CENTER_MFCD = { x = 11545 , y = 36 , width = 727, height = 750  }
+LEFT_MFCD = { x = 12363, y = 0 + 35 , width = 498, height = 538 }
+RIGHT_MFCD = { x = 11991, y = 829, width = 498, height = 538 }
+F18_RWR = { x = 11520+1024, y = 800 , width = 430, height = 450 }
 F18_UFC = { x = 11522  , y = 0, width = 2, height = 2 }
-F18_IFEI = { x = 11522, y =  3, width = 1, height = 1 }
+F18_IFEI = { x = 11520-480, y = 1420, width = 1480, height = 590 }
+
 
 Viewports =
 {
