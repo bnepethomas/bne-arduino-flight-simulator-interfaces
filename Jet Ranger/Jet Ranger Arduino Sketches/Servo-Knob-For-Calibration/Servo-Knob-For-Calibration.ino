0bbde9fd4c4d3049b4b2617972f192919ed37cbd
bnepethomas <bnepethomas@gmail.com>
2025-12-08 05:56:32 +1000

    Jet Ranger

---
+++ Jet Ranger/Jet Ranger Arduino Sketches/Servo-Knob-For-Calibration/Servo-Knob-For-Calibration.ino
@@ -0,0 +1,53 @@
+/*
+ Controlling a servo position using a potentiometer (variable resistor)
+ by Michal Rinott <http://people.interaction-ivrea.it/m.rinott>
+
+ modified on 8 Nov 2013
+ by Scott Fitzgerald
+ http://www.arduino.cc/en/Tutorial/Knob
+*/
+
+#include <Servo.h>
+
+Servo myservo;  // create Servo object to control a servo
+
+int lastpotchange = 0;
+int potpin = A0;  // analog pin used to connect the potentiometer
+int val;          // variable to read the value from the analog pin
+
+void setup() {
+
+  Serial.begin(9600);
+  myservo.attach(9);  // attaches the servo on pin 9 to the Servo object
+}
+
+void loop() {
+  val = analogRead(potpin);  // reads the value of the potentiometer (value between 0 and 1023)
+  if (val != lastpotchange) {
+    lastpotchange = val;
+    Serial.println(String(lastpotchange));
+  }
+  val = map(val, 1023, 0, 0, 180);  // scale it for use with the servo (value between 0 and 180)
+  myservo.write(val);               // sets the servo position according to the scaled value
+  delay(15);                        // waits for the servo to get there
+}
+
+void EngineTorque(int torque) {
+  int val = map(torque, 0, 120, 9, 809);
+  myservo.write(val);
+}
+
+void TransmissionPressure(int pressure) {
+  int val = map(pressure, 0, 150, 9, 288);
+  myservo.write(val);
+}
+
+void TransmissionTemperature(int temperature) {
+  int val = map(temperature, 0, 150, 424, 107);
+  myservo.write(val);
+}
+
+void GasProducer(int gaspercentage) {
+  int val = map(gaspercentage, 0, 106, 57, 848);
+  myservo.write(val);
+}
