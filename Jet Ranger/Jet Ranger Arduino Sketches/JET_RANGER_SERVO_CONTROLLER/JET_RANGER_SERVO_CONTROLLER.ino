c8982705a0a4b58558cd7fefdacbaa6de0a19ef7
bnepethomas <bnepethomas@gmail.com>
2026-01-10 16:14:03 +1000

    Jet Ranger

---
+++ Jet Ranger/Jet Ranger Arduino Sketches/JET_RANGER_SERVO_CONTROLLER/JET_RANGER_SERVO_CONTROLLER.ino
@@ -0,0 +1,143 @@
+/*
+
+  ////////////////////---||||||||||********||||||||||---\\\\\\\\\\\\\\\\\\\\
+  //||                  FUNCTION = Jet Ranger Servo                     ||\\
+  //||            ARDUINO PROCESSOR TYPE = Arduino Mega 2560            ||\\
+  //||      ARDUINO CHIP SERIAL NUMBER = SN -                           ||\\
+  //||                    CONNECTED COM PORT = COM                      ||\\
+  //||            ****DO CHECK S/N BEFORE UPLOAD NEW DATA****           ||\\
+  ////////////////////---||||||||||********||||||||||---\\\\\\\\\\\\\\\\\\\\
+
+
+
+*/
+#define GREEN_STATUS_LED_PORT 14
+#define RED_STATUS_LED_PORT 15  // RED LED is used for monitoring ethernet
+#define FLASH_TIME 200
+
+unsigned long NEXT_STATUS_TOGGLE_TIMER = 0;
+bool RED_LED_STATE = false;
+bool GREEN_LED_STATE = true;
+unsigned long timeSinceRedLedChanged = 0;
+
+#define Ethernet_In_Use 1
+
+
+int Reflector_In_Use = 1;
+
+// When Using Arduino Due this is not supported
+/*
+#define DCSBIOS_IRQ_SERIAL
+#include "DcsBios.h"
+*/
+
+// Ethernet Related
+#include <SPI.h>
+#include <Ethernet.h>
+#include <EthernetUdp.h>
+
+// These local Mac and IP Address will be reassigned early in startup based on
+// the device ID as set by address pins
+byte mac[] = { 0xA8, 0x61, 0x0A, 0x9E, 0x83, 0x01 };
+String sMac = "A8:61:0A:9E:83:01";
+IPAddress ip(172, 16, 1, 101);
+String strMyIP = "172.16.1.101";
+
+// Raspberry Pi is Target
+IPAddress reflectorIP(172, 16, 1, 10);
+String strreflectorIP = "X.X.X.X";
+
+
+
+
+const unsigned int localport = 7788;
+const unsigned int localdebugport = 7795;
+
+const unsigned int reflectorport = 27000;
+
+
+int packetSize;
+int debugLen;
+EthernetUDP udp;
+EthernetUDP debugUDP;
+
+#define EthernetStartupDelay 500
+#define ES1_RESET_PIN 53
+
+const unsigned long delayBeforeSendingPacket = 3000;
+unsigned long ethernetStartTime = 0;
+String BoardName = "Jet Ranger Servo: ";
+
+char packetBuffer[1000];     //buffer to store the incoming data
+char outpacketBuffer[1000];  //buffer to store the outgoing data
+
+void SendDebug(String MessageToSend) {
+  MessageToSend = BoardName + MessageToSend;
+  if ((Reflector_In_Use == 1) && (Ethernet_In_Use == 1)) {
+    udp.beginPacket(reflectorIP, reflectorport);
+    udp.print(MessageToSend);
+    udp.endPacket();
+  }
+}
+
+// ********************** Added Smoothing Filter for AOA Indexer Brightness
+// Not used in UIP combined controller
+// From https://github.com/jonnieZG/EWMA
+#include <Ewma.h>
+
+// ********************* End Smoothing Filter *************
+
+
+void setup() {
+
+  pinMode(GREEN_STATUS_LED_PORT, OUTPUT);
+  pinMode(RED_STATUS_LED_PORT, OUTPUT);
+  digitalWrite(GREEN_STATUS_LED_PORT, true);
+  digitalWrite(RED_STATUS_LED_PORT, false);
+
+  if (Ethernet_In_Use == 1) {
+
+    // Using manual reset instead of tying to Arduino Reset
+    pinMode(ES1_RESET_PIN, OUTPUT);
+    digitalWrite(ES1_RESET_PIN, LOW);
+    delay(2);
+    digitalWrite(ES1_RESET_PIN, HIGH);
+
+    Ethernet.begin(mac, ip);
+    udp.begin(localport);
+
+    ethernetStartTime = millis() + delayBeforeSendingPacket;
+    while (millis() <= ethernetStartTime) {
+      delay(FLASH_TIME);
+      digitalWrite(RED_STATUS_LED_PORT, false);
+      delay(FLASH_TIME);
+      digitalWrite(RED_STATUS_LED_PORT, true);
+    }
+
+    SendDebug("Ethernet Started " + strMyIP + " " + sMac);
+  }
+
+
+
+
+  SendDebug("Setup Complete");
+}
+
+#define ENG_OIL_TEMP_PORT 12
+#define ENG_OIL_TEMP_PORT 13
+#define ENG_TORQUE_PORT 11
+#define AIRSPEED_PORT 2
+#define GAS_PRODUCER_PORT 
+
+void loop() {
+
+  if (millis() >= NEXT_STATUS_TOGGLE_TIMER) {
+    GREEN_LED_STATE = !GREEN_LED_STATE;
+    RED_LED_STATE = !GREEN_LED_STATE;
+
+    digitalWrite(GREEN_STATUS_LED_PORT, GREEN_LED_STATE);
+    digitalWrite(RED_STATUS_LED_PORT, RED_LED_STATE);
+
+    NEXT_STATUS_TOGGLE_TIMER = millis() + FLASH_TIME;
+  }
+}
