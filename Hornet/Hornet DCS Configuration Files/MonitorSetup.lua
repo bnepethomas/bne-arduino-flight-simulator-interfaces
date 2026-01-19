939e75c0ffd1ed35c0655805f16b899ac6568629
bnepethomas <bnepethomas@gmail.com>
2026-01-20 06:58:29 +1100

    Update MonitorSetup.lua

--- Hornet/Hornet DCS Configuration Files/MonitorSetup.lua
+++ Hornet/Hornet DCS Configuration Files/MonitorSetup.lua
@@ -5,12 +5,15 @@ Description = 'Exported configuration with 3 projectors'
 -- First value is top left corner of the screen
 
 CENTER_MFCD = { x = 1600 ; y = 2160; width = 727; height = 750;}
-LEFT_MFCD = { x = 0; y = 2160; width = 755; height = 755;}
-RIGHT_MFCD = { x = 800; y = 2160; width = 755; height = 755; }
+DDIOFFSET = 10
+LEFT_MFCD = { x = 0+DDIOFFSET; y = 2160; width = 755; height = 755;}
+RIGHT_MFCD = { x = 800+DDIOFFSET; y = 2160; width = 755; height = 755; }
 --F18_RWR = { x = 11520+1024 + 25, y = 800 - 130 , width = 430, height = 450 }
 --F18_UFC = { x = 11522  , y = 2160, width = 2, height = 2 }
-F18_IFEI = { x = 3480; y = 2160; width = 1480; height = 595; }
---F18_HUD = { x = 2400, y = 2160, width = 800, height = 800*1.4}
+--F18_IFEI = { x = 3480; y = 2160; width = 1480; height = 595; }
+F18_IFEI = { x = 3005; y = 2180; width = 1480; height = 595; }
+HUDOFFSET=140
+F18_HUD = { x = 2400+HUDOFFSET; y = 2160; width = 800; height = 800*1.4;}
 
 Viewports =
 {
