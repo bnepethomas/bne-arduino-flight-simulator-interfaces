718d2224288ef7fc07776da19dd8de6e0346f193
bnepethomas <bnepethomas@gmail.com>
2026-05-10 16:07:42 +1000

    Hornet Health Monitor

---
+++ Hornet/C# Code/HornetHealthMonitor/Form1.Designer.cs
@@ -0,0 +1,151 @@
+﻿namespace MegaHealthMonitor
+{
+    partial class frmMain
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            listBoxLogs = new ListBox();
+            lblLeftConsole = new Label();
+            tmrHealthCheck = new System.Windows.Forms.Timer(components);
+            cmdToggleLogs = new Button();
+            lblUIP = new Label();
+            lblLIP = new Label();
+            lblLowerInst = new Label();
+            lblRightConsole = new Label();
+            SuspendLayout();
+            // 
+            // listBoxLogs
+            // 
+            listBoxLogs.FormattingEnabled = true;
+            listBoxLogs.Location = new Point(12, 67);
+            listBoxLogs.Name = "listBoxLogs";
+            listBoxLogs.Size = new Size(418, 199);
+            listBoxLogs.TabIndex = 0;
+            // 
+            // lblLeftConsole
+            // 
+            lblLeftConsole.AutoSize = true;
+            lblLeftConsole.BackColor = Color.White;
+            lblLeftConsole.ForeColor = SystemColors.ControlText;
+            lblLeftConsole.Location = new Point(12, 9);
+            lblLeftConsole.Name = "lblLeftConsole";
+            lblLeftConsole.Size = new Size(73, 15);
+            lblLeftConsole.TabIndex = 7;
+            lblLeftConsole.Text = "Left Console";
+            // 
+            // tmrHealthCheck
+            // 
+            tmrHealthCheck.Enabled = true;
+            tmrHealthCheck.Interval = 1000;
+            tmrHealthCheck.Tick += tmrHealthCheck_Tick;
+            // 
+            // cmdToggleLogs
+            // 
+            cmdToggleLogs.Location = new Point(12, 38);
+            cmdToggleLogs.Name = "cmdToggleLogs";
+            cmdToggleLogs.Size = new Size(75, 23);
+            cmdToggleLogs.TabIndex = 2;
+            cmdToggleLogs.Text = "Hide Logs";
+            cmdToggleLogs.UseVisualStyleBackColor = true;
+            cmdToggleLogs.Click += cmdToggleLogs_Click;
+            // 
+            // lblUIP
+            // 
+            lblUIP.AutoSize = true;
+            lblUIP.BackColor = Color.White;
+            lblUIP.ForeColor = SystemColors.ControlText;
+            lblUIP.Location = new Point(86, 9);
+            lblUIP.Name = "lblUIP";
+            lblUIP.Size = new Size(25, 15);
+            lblUIP.TabIndex = 3;
+            lblUIP.Text = "UIP";
+            // 
+            // lblLIP
+            // 
+            lblLIP.AutoSize = true;
+            lblLIP.BackColor = Color.White;
+            lblLIP.ForeColor = SystemColors.ControlText;
+            lblLIP.Location = new Point(117, 9);
+            lblLIP.Name = "lblLIP";
+            lblLIP.Size = new Size(23, 15);
+            lblLIP.TabIndex = 4;
+            lblLIP.Text = "LIP";
+            // 
+            // lblLowerInst
+            // 
+            lblLowerInst.AutoSize = true;
+            lblLowerInst.BackColor = Color.White;
+            lblLowerInst.ForeColor = SystemColors.ControlText;
+            lblLowerInst.Location = new Point(146, 9);
+            lblLowerInst.Name = "lblLowerInst";
+            lblLowerInst.Size = new Size(61, 15);
+            lblLowerInst.TabIndex = 5;
+            lblLowerInst.Text = "Lower Inst";
+            // 
+            // lblRightConsole
+            // 
+            lblRightConsole.AutoSize = true;
+            lblRightConsole.BackColor = Color.White;
+            lblRightConsole.ForeColor = SystemColors.ControlText;
+            lblRightConsole.Location = new Point(213, 9);
+            lblRightConsole.Name = "lblRightConsole";
+            lblRightConsole.Size = new Size(81, 15);
+            lblRightConsole.TabIndex = 6;
+            lblRightConsole.Text = "Right Console";
+            // 
+            // frmMain
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(465, 282);
+            Controls.Add(lblRightConsole);
+            Controls.Add(lblLowerInst);
+            Controls.Add(lblLIP);
+            Controls.Add(lblUIP);
+            Controls.Add(cmdToggleLogs);
+            Controls.Add(lblLeftConsole);
+            Controls.Add(listBoxLogs);
+            Name = "frmMain";
+            Text = "Hornet Health Monitor";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private ListBox listBoxLogs;
+        private Label lblLeftConsole;
+        private System.Windows.Forms.Timer tmrHealthCheck;
+        private Button cmdToggleLogs;
+        private Label lblUIP;
+        private Label lblLIP;
+        private Label lblLowerInst;
+        private Label lblRightConsole;
+    }
+}
---
+++ Hornet/C# Code/HornetHealthMonitor/Form1.cs
@@ -0,0 +1,304 @@
+using System;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MegaHealthMonitor
+{
+    public partial class frmMain : Form
+    {
+        private UdpClient udpServer;
+        private const int listenPort = 13137;
+
+        string UDP_Playload;
+        UdpClient udpClient = new UdpClient();
+
+        DateTime LeftConsoleLastReceived = DateTime.Now;
+        DateTime UIPLastReceived = DateTime.Now;
+        DateTime LIPLastReceived = DateTime.Now;
+        DateTime LowerInstLastReceived = DateTime.Now;
+        DateTime RightConsoleLastReceived = DateTime.Now;
+
+        bool loggingActive = true;
+
+        private void StartListener()
+        {
+            Task.Run(async () =>
+            {
+                try
+                {
+                    // Create UdpClient on the specified port
+                    using (udpServer = new UdpClient(listenPort))
+                    {
+                        while (true)
+                        {
+                            // Wait for incoming data
+                            UdpReceiveResult result = await udpServer.ReceiveAsync();
+                            string receivedData = Encoding.ASCII.GetString(result.Buffer);
+
+                            //if (loggingActive)
+                            //{
+                            //    this.Invoke(new Action(() =>
+                            //    {
+                            //        listBoxLogs.Items.Add($" Received: {receivedData} from {result.RemoteEndPoint}");
+
+                            //    }));
+                            //}
+
+                            if (receivedData.StartsWith("LEFT_CONSOLE"))
+                            {
+                                LeftConsoleLastReceived = DateTime.Now;
+                                // Update UI with received data (use Invoke to reach UI thread)
+                                if (loggingActive)
+                                {
+                                    this.Invoke(new Action(() =>
+                                    {
+                                        listBoxLogs.Items.Add(LeftConsoleLastReceived.Hour + ":" + LeftConsoleLastReceived.Minute + ":" + LeftConsoleLastReceived.Second + $" Received: {receivedData} from {result.RemoteEndPoint}");
+
+                                    }));
+                                }
+
+                                this.Invoke(new Action(() =>
+                                {
+                                    lblLeftConsole.BackColor = Color.Green;
+                                }));
+                            }
+                            else if (receivedData.StartsWith("UIP"))
+                            {
+                                UIPLastReceived = DateTime.Now;
+                                // Update UI with received data (use Invoke to reach UI thread)
+
+                                if (loggingActive)
+                                {
+                                    this.Invoke(new Action(() =>
+                                    {
+                                        listBoxLogs.Items.Add(UIPLastReceived.Hour + ":" + UIPLastReceived.Minute + ":" + UIPLastReceived.Second + $" Received: {receivedData} from {result.RemoteEndPoint}");
+                                    }));
+                                }
+                                this.Invoke(new Action(() =>
+                                {
+                                    lblUIP.BackColor = Color.Green;
+                                }));
+
+                            }
+                            else if (receivedData.StartsWith("LIP"))
+                            {
+                                LIPLastReceived = DateTime.Now;
+                                // Update UI with received data (use Invoke to reach UI thread)
+                                if (loggingActive)
+                                {
+                                    this.Invoke(new Action(() =>
+                                    {
+                                        listBoxLogs.Items.Add(LIPLastReceived.Hour + ":" + LIPLastReceived.Minute + ":" + LIPLastReceived.Second + $" Received: {receivedData} from {result.RemoteEndPoint}");
+                                    }));
+                                }
+                                this.Invoke(new Action(() =>
+                                {
+                                    lblLIP.BackColor = Color.Green;
+                                }));
+                            }
+                            else if (receivedData.StartsWith("LOWER_INST"))
+                            {
+                                LowerInstLastReceived = DateTime.Now;
+                                // Update UI with received data (use Invoke to reach UI thread)
+                                if (loggingActive)
+                                {
+                                    this.Invoke(new Action(() =>
+                                    {
+                                        listBoxLogs.Items.Add(LowerInstLastReceived.Hour + ":" + LowerInstLastReceived.Minute + ":" + LowerInstLastReceived.Second + $" Received: {receivedData} from {result.RemoteEndPoint}");
+                                    }));
+                                }
+                                this.Invoke(new Action(() =>
+                                {
+                                    lblLowerInst.BackColor = Color.Green;
+                                }));
+                            }
+                            else if (receivedData.StartsWith("RIGHT_CONSOLE"))
+                            {
+                                RightConsoleLastReceived = DateTime.Now;
+                                // Update UI with received data (use Invoke to reach UI thread)
+                                if (loggingActive)
+                                {
+                                    this.Invoke(new Action(() =>
+                                    {
+                                        listBoxLogs.Items.Add(RightConsoleLastReceived.Hour + ":" + RightConsoleLastReceived.Minute + ":" + RightConsoleLastReceived.Second + $" Received: {receivedData} from {result.RemoteEndPoint}");
+                                    }));
+                                }
+                                this.Invoke(new Action(() =>
+                                {
+                                    lblRightConsole.BackColor = Color.Green;
+                                }));
+                            }
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    // Handle socket exceptions (e.g., if port is already in use)
+                    Console.WriteLine(ex.Message);
+                }
+            });
+        }
+
+
+        private void CheckHealth()
+        {
+            TimeSpan span = DateTime.Now - LeftConsoleLastReceived;
+            int mS = (int)span.TotalMilliseconds;
+            // displayText("Its been this many mS since sending last packet: " + mS.ToString());
+
+            if (mS >= 30000)
+            {
+                if (lblLeftConsole.BackColor != Color.Red)
+                {
+                    AddLog("Left Console connection lost.");
+                }
+                lblLeftConsole.BackColor = Color.Red;
+
+            }
+            else if (mS >= 15000)
+            {
+                if (lblLeftConsole.BackColor != Color.Orange)
+                {
+                    AddLog("Left Console connection warning.");
+                }
+                lblLeftConsole.BackColor = Color.Orange;
+            }
+
+            span = DateTime.Now - UIPLastReceived;
+            mS = (int)span.TotalMilliseconds;
+            // displayText("Its been this many mS since sending last packet: " + mS.ToString());
+
+            if (mS >= 30000)
+            {
+                if (lblUIP.BackColor != Color.Red)
+                {
+                    AddLog("UIP connection lost.");
+                }
+                lblUIP.BackColor = Color.Red;
+
+            }
+            else if (mS >= 15000)
+            {
+                if (lblUIP.BackColor != Color.Orange)
+                {
+                    AddLog("UIP connection warning.");
+                }
+                lblUIP.BackColor = Color.Orange;
+            }
+
+            span = DateTime.Now - LIPLastReceived;
+            mS = (int)span.TotalMilliseconds;
+
+            if (mS >= 30000)
+            {
+                if (lblLIP.BackColor != Color.Red)
+                {
+                    AddLog("LIP connection lost.");
+                }
+                lblLIP.BackColor = Color.Red;
+            }
+            else if (mS >= 15000)
+            {
+                if (lblLIP.BackColor != Color.Orange)
+                {
+                    AddLog("LIP connection warning.");
+                }
+                lblLIP.BackColor = Color.Orange;
+            }
+
+            span = DateTime.Now - LowerInstLastReceived;
+            mS = (int)span.TotalMilliseconds;
+
+            if (mS >= 30000)
+            {
+                if (lblLowerInst.BackColor != Color.Red)
+                {
+                    AddLog("Lower Inst connection lost.");
+                }
+                lblLowerInst.BackColor = Color.Red;
+            }
+            else if (mS >= 15000)
+            {
+                if (lblLowerInst.BackColor != Color.Orange)
+                {
+                    AddLog("Lower Inst connection warning.");
+                }
+                lblLowerInst.BackColor = Color.Orange;
+            }
+
+            span = DateTime.Now - RightConsoleLastReceived;
+            mS = (int)span.TotalMilliseconds;
+
+            if (mS >= 30000)
+            {
+                if (lblRightConsole.BackColor != Color.Red)
+                {
+                    AddLog("Right Console connection lost.");
+                }
+                lblRightConsole.BackColor = Color.Red;
+            }
+            else if (mS >= 15000)
+            {
+                if (lblRightConsole.BackColor != Color.Orange)
+                {
+                    AddLog("Right Console connection warning.");
+                }
+                lblRightConsole.BackColor = Color.Orange;
+            }
+
+
+        }
+
+        private void AddLog(string logMessage)
+        {
+            if (loggingActive)
+            {
+                listBoxLogs.Items.Add(DateTime.Now.Hour + ":" + DateTime.Now.Minute + ":" + DateTime.Now.Second + " " + logMessage);
+            }
+        }
+
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            StartListener();
+        }
+
+
+        private void tmrHealthCheck_Tick(object sender, EventArgs e)
+        {
+            CheckHealth();
+        }
+
+        private void cmdToggleLogs_Click(object sender, EventArgs e)
+        {
+            if (listBoxLogs.Visible == true)
+            {
+                listBoxLogs.Items.Clear();
+                listBoxLogs.Visible = false;
+                cmdToggleLogs.Text = "Show Logs";
+                this.Height = this.Height - listBoxLogs.Size.Height - 20;
+                this.Width = this.Width - 300;
+            }
+
+            else
+            {
+                listBoxLogs.Visible = true;
+                cmdToggleLogs.Text = "Hide Logs";
+                this.Height = this.Height + listBoxLogs.Size.Height + 20;
+                this.Width = this.Width + 300;
+
+            }
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+}
