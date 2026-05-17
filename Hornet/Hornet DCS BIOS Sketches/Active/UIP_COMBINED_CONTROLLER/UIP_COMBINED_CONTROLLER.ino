e1f6e67bbbd8679d1380578739112473fed8de77
bnepethomas <bnepethomas@gmail.com>
2026-05-17 14:41:36 +1000

    compass

--- Hornet/Hornet DCS BIOS Sketches/Active/UIP_COMBINED_CONTROLLER/UIP_COMBINED_CONTROLLER.ino
+++ Hornet/Hornet DCS BIOS Sketches/Active/UIP_COMBINED_CONTROLLER/UIP_COMBINED_CONTROLLER.ino
@@ -947,7 +947,7 @@ Vid60Stepper standbyCompass(0x0436,         // address of stepper data
                             stepper,        // name of AccelStepper instance
                             stepperConfig,  // StepperConfig struct instance
                             16,             // IR Detector Pin (must be LOW in zero position)
-                            585,            // zero offset
+                            480,            // zero offset
                             [](unsigned int newValue) -> unsigned int {
                               /* this function needs to map newValue to the correct number of steps */
 
