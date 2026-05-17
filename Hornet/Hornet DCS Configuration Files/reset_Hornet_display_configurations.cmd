f3afef26a2b0c1cd98fcef34dbe20a4920541932
bnepethomas <bnepethomas@gmail.com>
2026-05-17 11:22:49 +1000

    hud

--- Hornet/Hornet DCS Configuration Files/reset_Hornet_display_configurations.cmd
+++ Hornet/Hornet DCS Configuration Files/reset_Hornet_display_configurations.cmd
@@ -4,5 +4,7 @@ echo CDU
 copy .\IFEI_init.lua "D:\Program Files\Eagle Dynamics\DCS World\Mods\aircraft\FA-18C\Cockpit\Scripts\IFEI\indicator"
 copy .\UFC_init.lua "D:\Program Files\Eagle Dynamics\DCS World\Mods\aircraft\FA-18C\Cockpit\Scripts\UFC\indicator"
 copy .\RWR_ALR67_init.lua "D:\Program Files\Eagle Dynamics\DCS World\Mods\aircraft\FA-18C\Cockpit\Scripts\TEWS\indicator"
-copy .\AVQ32_init.lua "D:\Program Files\Eagle Dynamics\DCS World\Mods\aircraft\FA-18C\Cockpit\Scripts\Multipurpose_Display_Group\HUD_AVQ32\indicator\"                
+copy .\AVQ32_init.lua "D:\Program Files\Eagle Dynamics\DCS World\Mods\aircraft\FA-18C\Cockpit\Scripts\Multipurpose_Display_Group\HUD_AVQ32\indicator\"    
+copy .\FONTS.LUA "D:\Program Files\Eagle Dynamics\DCS World\Mods\aircraft\FA-18C\Cockpit\Scripts\fonts.lua"
+copy .\materials.lua "D:\Program Files\Eagle Dynamics\DCS World\Mods\aircraft\FA-18C\Cockpit\Scripts\materials.lua"            
 pause
