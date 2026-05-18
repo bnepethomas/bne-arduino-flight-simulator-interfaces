ff3488d240fbc758eaeed9632278ffade84c0c4b
bnepethomas <bnepethomas@gmail.com>
2026-05-18 16:37:46 +1000

    Create multikey.txt.ino

---
+++ Hornet/Hornet DCS BIOS Sketches/Active/RIGHT_CONSOLE_SIM_CONTROL/multikey.txt/multikey.txt.ino
@@ -0,0 +1,78 @@
+/* @file MultiKey.ino
+|| @version 1.0
+|| @author Mark Stanley
+|| @contact mstanley@technologist.com
+||
+|| @description
+|| | The latest version, 3.0, of the keypad library supports up to 10
+|| | active keys all being pressed at the same time. This sketch is an
+|| | example of how you can get multiple key presses from a keypad or
+|| | keyboard.
+|| #
+*/
+
+#include <Keypad.h>
+
+
+const byte ROWS = 2;
+const byte COLS = 8;
+
+byte rowPins[ROWS] = { A0, A1 };
+byte colPins[COLS] = { 4, 5, 6, 7, 8, 9, 10, 11 };
+
+char keys[ROWS][COLS] = {
+  { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h' },
+  { '1', '2', '3', '4', '5', '6', '7', '8' }
+};
+Keypad kpd = Keypad( makeKeymap(keys), rowPins, colPins, ROWS, COLS );
+
+unsigned long loopCount;
+unsigned long startTime;
+String msg;
+
+
+void setup() {
+    Serial.begin(9600);
+    loopCount = 0;
+    startTime = millis();
+    msg = "";
+}
+
+
+void loop() {
+    loopCount++;
+    if ( (millis()-startTime)>5000 ) {
+        Serial.print("Average loops per second = ");
+        Serial.println(loopCount/5);
+        startTime = millis();
+        loopCount = 0;
+    }
+
+    // Fills kpd.key[ ] array with up-to 10 active keys.
+    // Returns true if there are ANY active keys.
+    if (kpd.getKeys())
+    {
+        for (int i=0; i<LIST_MAX; i++)   // Scan the whole key list.
+        {
+            if ( kpd.key[i].stateChanged )   // Only find keys that have changed state.
+            {
+                switch (kpd.key[i].kstate) {  // Report active key state : IDLE, PRESSED, HOLD, or RELEASED
+                    case PRESSED:
+                    msg = " PRESSED.";
+                break;
+                    case HOLD:
+                    msg = " HOLD.";
+                break;
+                    case RELEASED:
+                    msg = " RELEASED.";
+                break;
+                    case IDLE:
+                    msg = " IDLE.";
+                }
+                Serial.print("Key ");
+                Serial.print(kpd.key[i].kchar);
+                Serial.println(msg);
+            }
+        }
+    }
+}  // End loop
