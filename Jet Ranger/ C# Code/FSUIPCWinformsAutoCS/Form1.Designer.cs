64dfbbc9c922d049a04b4b183470c2f853db4253
bnepethomas <bnepethomas@gmail.com>
2026-07-09 05:50:44 +1000

    fsuipc

---
+++ Jet Ranger/ C# Code/FSUIPCWinformsAutoCS/Form1.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace FSUIPCTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            statusStrip = new StatusStrip();
+            lblConnectionStatus = new ToolStripStatusLabel();
+            timerMain = new System.Windows.Forms.Timer(components);
+            txtAirspeed = new TextBox();
+            chkAvionicsMaster = new CheckBox();
+            label1 = new Label();
+            timerConnection = new System.Windows.Forms.Timer(components);
+            label2 = new Label();
+            txtGasProducer = new TextBox();
+            statusStrip.SuspendLayout();
+            SuspendLayout();
+            // 
+            // statusStrip
+            // 
+            statusStrip.Items.AddRange(new ToolStripItem[] { lblConnectionStatus });
+            statusStrip.Location = new Point(0, 344);
+            statusStrip.Name = "statusStrip";
+            statusStrip.Padding = new Padding(1, 0, 16, 0);
+            statusStrip.Size = new Size(502, 22);
+            statusStrip.TabIndex = 6;
+            statusStrip.Text = "statusStrip1";
+            // 
+            // lblConnectionStatus
+            // 
+            lblConnectionStatus.Name = "lblConnectionStatus";
+            lblConnectionStatus.Size = new Size(104, 17);
+            lblConnectionStatus.Text = "Connection Status";
+            // 
+            // timerMain
+            // 
+            timerMain.Tick += timerMain_Tick;
+            // 
+            // txtAirspeed
+            // 
+            txtAirspeed.Location = new Point(187, 14);
+            txtAirspeed.Margin = new Padding(4, 3, 4, 3);
+            txtAirspeed.Name = "txtAirspeed";
+            txtAirspeed.ReadOnly = true;
+            txtAirspeed.Size = new Size(116, 23);
+            txtAirspeed.TabIndex = 9;
+            // 
+            // chkAvionicsMaster
+            // 
+            chkAvionicsMaster.CheckAlign = ContentAlignment.MiddleRight;
+            chkAvionicsMaster.Location = new Point(13, 46);
+            chkAvionicsMaster.Margin = new Padding(4, 3, 4, 3);
+            chkAvionicsMaster.Name = "chkAvionicsMaster";
+            chkAvionicsMaster.Size = new Size(191, 28);
+            chkAvionicsMaster.TabIndex = 8;
+            chkAvionicsMaster.Text = "Avionics Master";
+            chkAvionicsMaster.UseVisualStyleBackColor = true;
+            chkAvionicsMaster.CheckedChanged += chkAvionicsMaster_CheckedChanged;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(13, 17);
+            label1.Margin = new Padding(4, 0, 4, 0);
+            label1.Name = "label1";
+            label1.Size = new Size(141, 15);
+            label1.TabIndex = 7;
+            label1.Text = "Indicated Airpeed (Knots)";
+            // 
+            // timerConnection
+            // 
+            timerConnection.Interval = 1000;
+            timerConnection.Tick += timerConnection_Tick;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(13, 89);
+            label2.Margin = new Padding(4, 0, 4, 0);
+            label2.Name = "label2";
+            label2.Size = new Size(90, 15);
+            label2.TabIndex = 11;
+            label2.Text = "Gas Producer %";
+            // 
+            // txtGasProducer
+            // 
+            txtGasProducer.Location = new Point(187, 81);
+            txtGasProducer.Margin = new Padding(4, 3, 4, 3);
+            txtGasProducer.Name = "txtGasProducer";
+            txtGasProducer.ReadOnly = true;
+            txtGasProducer.Size = new Size(116, 23);
+            txtGasProducer.TabIndex = 12;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(502, 366);
+            Controls.Add(txtGasProducer);
+            Controls.Add(label2);
+            Controls.Add(statusStrip);
+            Controls.Add(txtAirspeed);
+            Controls.Add(chkAvionicsMaster);
+            Controls.Add(label1);
+            Margin = new Padding(4, 3, 4, 3);
+            Name = "Form1";
+            Text = "Main Form";
+            FormClosing += frmMain_FormClosing;
+            statusStrip.ResumeLayout(false);
+            statusStrip.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.StatusStrip statusStrip;
+        private System.Windows.Forms.ToolStripStatusLabel lblConnectionStatus;
+        private System.Windows.Forms.Timer timerMain;
+        private System.Windows.Forms.TextBox txtAirspeed;
+        private System.Windows.Forms.CheckBox chkAvionicsMaster;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timerConnection;
+        private Label label2;
+        private TextBox txtGasProducer;
+    }
+}
+
---
+++ Jet Ranger/ C# Code/FSUIPCWinformsAutoCS/Form1.cs
@@ -0,0 +1,118 @@
+﻿using FSUIPC;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FSUIPCTest
+{
+    public partial class Form1 : Form
+    {
+        // =====================================
+        // DECLARE OFFSETS YOU WANT TO USE HERE
+        // =====================================
+        private Offset<uint> airspeed = new Offset<uint>(0x02BC);
+        private Offset<uint> avionicsMaster = new Offset<uint>(0x2E80);
+        private Offset<uint> gasProducer = new Offset<uint>(0x0898);
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            configureForm();
+            // Start the connection timer to look for a flight sim
+            this.timerConnection.Start();
+        }
+
+        // This method is called every 1 second by the connection timer.
+        private void timerConnection_Tick(object sender, EventArgs e)
+        {
+            // Try to open the connection
+            try
+            {
+                FSUIPCConnection.Open();
+                // If there was no problem, stop this timer and start the main timer
+                this.timerConnection.Stop();
+                this.timerMain.Start();
+                // Update the connection status
+                configureForm();
+            }
+            catch
+            {
+                // No connection found. Don't need to do anything, just keep trying
+            }
+        }
+
+        // This method runs 20 times per second (every 50ms). This is set on the timerMain properties.
+        private void timerMain_Tick(object sender, EventArgs e)
+        {
+            // Call process() to read/write data to/from FSUIPC
+            // We do this in a Try/Catch block incase something goes wrong
+            try
+            {
+                FSUIPCConnection.Process();
+
+                // Update the information on the form
+                // (See the Examples Application for more information on using Offsets).
+
+                // 1. Airspeed
+                double airspeedKnots = (double)this.airspeed.Value / 128d;
+                this.txtAirspeed.Text = airspeedKnots.ToString("F0");
+
+                // 2. Master Avionics
+                this.chkAvionicsMaster.Checked = avionicsMaster.Value > 0;
+
+                // 3. Gas Producer
+                double gasProducerPercent = (double)this.gasProducer.Value / 16384 *100;
+                this.txtGasProducer.Text =  gasProducerPercent.ToString("F1") + "%";
+            }
+            catch (Exception ex)
+            {
+                // An error occured. Tell the user and stop this timer.
+                this.timerMain.Stop();
+                MessageBox.Show("Communication with FSUIPC Failed\n\n" + ex.Message, "FSUIPC", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                // Update the connection status
+                configureForm();
+                // start the connection timer
+                this.timerConnection.Start();
+            }
+        }
+
+        // This runs when the master avionics tick has been changed
+        private void chkAvionicsMaster_CheckedChanged(object sender, EventArgs e)
+        {
+            // Update the FSUIPC offset with the new value (1 = Checked/On, 0 = Unchecked/Off)
+            this.avionicsMaster.Value = (uint)(this.chkAvionicsMaster.Checked ? 1 : 0);
+        }
+
+        // Configures the status label depending on if we're connected or not 
+        private void configureForm()
+        {
+            if (FSUIPCConnection.IsOpen)
+            {
+                this.lblConnectionStatus.Text = "Connected to " + FSUIPCConnection.FlightSimVersionConnected.ToString();
+                this.lblConnectionStatus.ForeColor = Color.Green;
+            }
+            else
+            {
+                this.lblConnectionStatus.Text = "Disconnected. Looking for Flight Simulator...";
+                this.lblConnectionStatus.ForeColor = Color.Red;
+            }
+        }
+
+        // Form is closing so stop all the timers and close FSUIPC Connection
+        private void frmMain_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            this.timerConnection.Stop();
+            this.timerMain.Stop();
+            FSUIPCConnection.Close();
+        }
+
+
+    }
+}
